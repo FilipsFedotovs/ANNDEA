7d0ceadaaa50937949832802c6b481efed293469
Filips <fedotovs@hotmail.com>
2022-10-20 14:47:00 +0100

    Issue #7 - Continuing the segment merger development

--- Code/Utilities/UtilityFunctions.py
+++ Code/Utilities/UtilityFunctions.py
@@ -1285,7 +1285,6 @@ def CreateCondorJobs(AFS,EOS,path,o,pfx,sfx,ID,loop_params,OptionHeader,OptionLi
     if batch_sub==False:
         from alive_progress import alive_bar
         bad_pop=[]
-        print(loop_params)
         TotJobs=0
         if type(loop_params) is int:
             nest_lvl=1
