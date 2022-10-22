1545e1876616f011d935cf18444c0fb16c8cd8f9
Filips <fedotovs@hotmail.com>
2022-10-22 15:29:02 +0100

    Issue #7 - Investigating Sample Generation bug

--- Code/MUTr1_GenerateTrainUnionSeeds.py
+++ Code/MUTr1_GenerateTrainUnionSeeds.py
@@ -672,6 +672,7 @@ while status<7:
                  JobSet[i].append(JobSets[i][3][j])
 
            for i in range(0,len(JobSet)):
+               print('Here')
                input_file_location=EOS_DIR+'/ANNADEA/Data/TRAIN_SET/MUTr1d_'+TrainSampleID+'_SampledCompressedSeeds_'+str(i)+'.pkl'
                if os.path.isfile(input_file_location):
                   base_data=UF.PickleOperations(input_file_location,'r','N/A')
