bcdc0902a5fb50046d0f3aa2bb75ca52bdc80cb1
Filips <fedotovs@hotmail.com>
2022-10-22 19:17:42 +0100

    Issue #8 - Model Training File dev

--- Code/Utilities/UtilityFunctions.py
+++ Code/Utilities/UtilityFunctions.py
@@ -1578,3 +1578,28 @@ def ErrorOperations(a,b,a_e,b_e,mode):
     if mode=='/':
         c_e=(a/b)*math.sqrt(((a_e/a)**2) + ((b_e/b)**2))
         return(c_e)
+
+def LoadRenderImages(Tracks,StartTrack,EndTrack,numClasses):
+    import tensorflow as tf
+    from tensorflow import keras
+    NewTracks=Tracks[StartTrack-1:min(EndTrack,len(Tracks))]
+    ImagesY=np.empty([len(NewTracks),1])
+    ImagesX=np.empty([len(NewTracks),NewTracks[0].H,NewTracks[0].W,NewTracks[0].L],dtype=np.bool)
+    for im in range(len(NewTracks)):
+        if hasattr(NewTracks[im],'MC_truth_label'):
+           ImagesY[im]=int(float(NewTracks[im].MC_truth_label))
+        else:
+           ImagesY[im]=0
+        BlankRenderedImage=[]
+        for x in range(-NewTracks[im].bX,NewTracks[im].bX):
+          for y in range(-NewTracks[im].bY,NewTracks[im].bY):
+            for z in range(0,NewTracks[im].bZ):
+             BlankRenderedImage.append(0)
+        RenderedImage = np.array(BlankRenderedImage)
+        RenderedImage = np.reshape(RenderedImage,(NewTracks[im].H,NewTracks[im].W,NewTracks[im].L))
+        for Hits in NewTracks[im].TrackPrint:
+                   RenderedImage[Hits[0]+NewTracks[im].bX][Hits[1]+NewTracks[im].bY][Hits[2]]=1
+        ImagesX[im]=RenderedImage
+    ImagesX= ImagesX[..., np.newaxis]
+    ImagesY=tf.keras.utils.to_categorical(ImagesY,numClasses)
+    return (ImagesX,ImagesY)
