87940199f25030811f0aad32e9321c2691d4e213
Filips <fedotovs@hotmail.com>
2022-10-23 14:12:45 +0100

    Issue #8 - Model Training File dev

--- Code/Utilities/MUTr2_TrainModel_Sub.py
+++ Code/Utilities/MUTr2_TrainModel_Sub.py
@@ -8,13 +8,12 @@
 import argparse
 import math
 import ast
-import numpy as np
-import torch
-from torch import optim
-from torch.optim.lr_scheduler import StepLR
-import torch.nn.functional as F
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-device = "cpu"
+# import torch
+# from torch import optim
+# from torch.optim.lr_scheduler import StepLR
+# import torch.nn.functional as F
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# device = "cpu"
 ########################## Visual Formatting #################################################
 class bcolors:
     HEADER = '\033[95m'
@@ -28,7 +27,6 @@ class bcolors:
 
 ########################## Setting the parser ################################################
 parser = argparse.ArgumentParser(description='select cut parameters')
-parser.add_argument('--ModelParams',help="Please enter the model params: '[<Number of MLP layers>, <'MLP hidden size'>, <Number of IN layers>, <'IN hidden size'>]'", default='[3,80,3,80]')
 parser.add_argument('--TrainParams',help="Please enter the train params: '[<Session No>, <Learning Rate>, <Batch size>, <Epochs>]'", default='[1, 0.0001, 4, 10]')
 parser.add_argument('--AFS',help="Please enter the user afs directory", default='.')
 parser.add_argument('--EOS',help="Please enter the user eos directory", default='.')
@@ -39,11 +37,8 @@ parser.add_argument('--p',help="Path to the output file", default='')
 parser.add_argument('--o',help="Path to the output file name", default='')
 parser.add_argument('--pfx',help="Path to the output file name", default='')
 parser.add_argument('--sfx',help="Path to the output file name", default='')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
 ########################################     Initialising Variables    #########################################
 args = parser.parse_args()
-ModelParams=ast.literal_eval(args.ModelParams)
 TrainParams=ast.literal_eval(args.TrainParams)
 TrainSampleID=args.BatchID
 ModelName=args.ModelName
@@ -69,55 +64,17 @@ print(UF.TimeStamp(), bcolors.OKGREEN+"Modules Have been imported successfully..
 def zero_divide(a, b):
     if (b==0): return 0
     return a/b
-def binary_classification_stats(output, y, thld):
-    TP = torch.sum((y==1) & (output>thld))
-    TN = torch.sum((y==0) & (output<thld))
-    FP = torch.sum((y==0) & (output>thld))
-    FN = torch.sum((y==1) & (output<thld))
-    acc = zero_divide(TP+TN, TP+TN+FP+FN)
-    TPR = zero_divide(TP, TP+FN)
-    TNR = zero_divide(TN, TN+FP)
-    return acc, TPR, TNR
 
-def train(model, device, sample, optimizer):
-    """ train routine, loss and accumulated gradients used to update
-        the model via the ADAM optimizer externally
-    """
-    model.train()
-    losses_w = [] # edge weight loss
-    iterator=0
-    for HC in sample:
-        data = HC.to(device)
-        if (len(data.x)==0 or len(data.edge_index)==0): continue
-
-#        try:
-        iterator+=1
-        w = model(data.x, data.edge_index, data.edge_attr)
-        y, w = data.y.float(), w.squeeze(1)
-        # except:
-        #     print('Erroneus data set: ',data.x, data.edge_index, data.edge_attr, 'skipping these samples...')
-        #     continue
-        #edge weight loss
-        loss_w = F.binary_cross_entropy(w, y, reduction='mean')
-        # optimize total loss
-        if iterator%TrainParams[1]==0:
-           optimizer.zero_grad()
-           loss_w.backward()
-           optimizer.step()
-
-        # store losses
-        losses_w.append(loss_w.item())
-    loss_w = np.nanmean(losses_w)
-    return loss_w,iterator
 def CNNtrain(model, Sample, Batches):
-    iterator=0
+
     for ib in range(0,Batches):
         StartSeed=(ib*TrainParams[1])+1
         EndSeed=StartSeed+TrainParams[1]-1
-        iterator+=TrainParams[1]
+
         BatchImages=UF.LoadRenderImages(Sample,StartSeed,EndSeed)
         t=model.train_on_batch(BatchImages[0],BatchImages[1],reset_metrics=False)
-    return t,iterator
+        print(ib,t)
+    return t
 
 def CNNvalidate(model, Sample, Batches):
     for ib in range(0,Batches):
@@ -125,6 +82,7 @@ def CNNvalidate(model, Sample, Batches):
         EndSeed=StartSeed+TrainParams[1]-1
         BatchImages=UF.LoadRenderImages(Sample,StartSeed,EndSeed)
         v=model.test_on_batch(BatchImages[0],BatchImages[1],reset_metrics=False)
+        print(ib,v)
     return v
 
 
@@ -149,22 +107,7 @@ if ModelMeta.ModelType=='CNN':
    NValBatches=math.ceil(float(len(ValSamples))/float(TrainParams[2]))
    for ts in TrainSamples:
        ts.PrepareTrackPrint(ModelMeta,'CNN-E')
-# for i in range(1,Meta.no_sets+1):
-#         flocation=EOS_DIR+'/ANNADEA/Data/TRAIN_SET/'+TrainSampleID+'_TH_OUTPUT_'+str(i)+'.pkl'
-#         print(UF.TimeStamp(),'Loading data from ',bcolors.OKBLUE+flocation+bcolors.ENDC)
-#         TrainClusters=UF.PickleOperations(flocation,'r', 'N/A')
-#         TrainClusters=TrainClusters[0]
-#         TrainFraction=int(math.floor(len(TrainClusters)*(1.0-(Meta.testRatio+Meta.valRatio))))
-#         ValFraction=int(math.ceil(len(TrainClusters)*Meta.valRatio))
-#         for smpl in range(0,TrainFraction):
-#            if TrainClusters[smpl].ClusterGraph.num_edges>0:
-#              TrainSamples.append(TrainClusters[smpl].ClusterGraph)
-#         for smpl in range(TrainFraction,TrainFraction+ValFraction):
-#             if TrainClusters[smpl].ClusterGraph.num_edges>0:
-#              ValSamples.append(TrainClusters[smpl].ClusterGraph)
-#         for smpl in range(TrainFraction+ValFraction,len(TrainClusters)):
-#             if TrainClusters[smpl].ClusterGraph.num_edges>0:
-#              TestSamples.append(TrainClusters[smpl].ClusterGraph)
+
 print(UF.TimeStamp(), bcolors.OKGREEN+"Train and Validation data has loaded and analysed successfully..."+bcolors.ENDC)
 
 def main(self):
@@ -189,7 +132,7 @@ def main(self):
         model.summary()
         records=[]
         for epoch in range(0, TrainParams[2]):
-            train_loss, itr=CNNtrain(model, TrainSamples, NTrainBatches)
+            train_loss, itr=CNNtrain(model, TrainSamples, NTrainBatches),len(TrainSamples)
             val_loss=CNNvalidate(model, ValSamples, NValBatches)
             test_loss=val_loss
             print(UF.TimeStamp(),'Epoch ',epoch, ' is completed')
@@ -202,34 +145,6 @@ def main(self):
         ModelMeta.CompleteTrainingSession(Header)
         print(UF.PickleOperations(Model_Meta_Path, 'w', ModelMeta)[1])
         exit()
-    # optimizer = optim.Adam(model.parameters(), lr=TrainParams[0])
-    # scheduler = StepLR(optimizer, step_size=0.1,gamma=0.1)
-    # print(UF.TimeStamp(),'Try to load the previously saved model/optimiser state files ')
-    # try:
-    #        model.load_state_dict(torch.load(Model_Path))
-    #        checkpoint = torch.load(State_Save_Path)
-    #        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    #        scheduler.load_state_dict(checkpoint['scheduler'])
-    # except:
-    #        print(UF.TimeStamp(), bcolors.WARNING+"Model/state data files are missing, skipping this step..." +bcolors.ENDC)
-    # records=[]
-    # for epoch in range(0, TrainParams[2]):
-    #      train_loss, itr= train(model, device,TrainSamples, optimizer)
-    #      thld, val_loss,val_acc = validate(model, device, ValSamples)
-    #      test_loss, test_acc = test(model, device,TestSamples, thld)
-    #      scheduler.step()
-    #      print(UF.TimeStamp(),'Epoch ',epoch, ' is completed')
-    #      records.append([epoch,itr,train_loss,thld,val_loss,val_acc,test_loss,test_acc])
-    #      torch.save({    'epoch': epoch,
-    #                   'optimizer_state_dict': optimizer.state_dict(),
-    #                   'scheduler': scheduler.state_dict(),    # HERE IS THE CHANGE
-    #                   }, State_Save_Path)
-    # torch.save(model.state_dict(), Model_Path)
-    # Header=[['Epoch','# Samples','Train Loss','Optimal Threshold','Validation Loss','Validation Accuracy','Test Loss','Test Accuracy']]
-    # Header+=records
-    # ModelMeta.CompleteTrainingSession(Header)
-    # print(UF.PickleOperations(Model_Meta_Path, 'w', ModelMeta)[1])
-    # exit()
 if __name__ == '__main__':
      main(sys.argv[1:])
 
