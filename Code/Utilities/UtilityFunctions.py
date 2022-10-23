6dfac6085d0f331396faca3e27be0b44b0ddcd89
Filips <fedotovs@hotmail.com>
2022-10-23 20:22:37 +0100

    Issue #8 - Model Training File dev

--- Code/Utilities/UtilityFunctions.py
+++ Code/Utilities/UtilityFunctions.py
@@ -1586,9 +1586,10 @@ def GenerateModel(ModelMeta,TrainParams=None):
                 def forward(self, x, edge_index, edge_attr, batch):
                     # 1. Obtain node embeddings
                     if len(HiddenLayer)==3:
+
+                        x = self.conv1(x, edge_index, edge_attr)
                         print(x)
                         exit()
-                        x = self.conv1(x, edge_index, edge_attr)
                         x = x.relu()
                         x = self.conv2(x, edge_index, edge_attr)
                         x = x.relu()
