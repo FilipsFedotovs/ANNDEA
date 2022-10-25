632814eb976675d2f1871b5511cc77fc0ec19d8b
Filips <fedotovs@hotmail.com>
2022-10-25 22:21:39 +0100

    Issue #9 - Developing the Track Merger reconstruction bit

--- Code/RUTr1_MergeTracks.py
+++ Code/RUTr1_MergeTracks.py
@@ -217,12 +217,12 @@ else:
    MaxSegments=PM.MaxSegments
    MaxSeeds=PM.MaxSeeds
    VetoMotherTrack=PM.VetoMotherTrack
-   exit()
+
 # TotJobs=0
 # for j in range(0,len(JobSets)):
 #           for sj in range(0,int(JobSets[j][2])):
 #               TotJobs+=1
-exit()
+
 ########################################     Preset framework parameters    #########################################
 FreshStart=True
 def AutoPilot(wait_min, interval_min, max_interval_tolerance,AFS,EOS,path,o,pfx,sfx,ID,loop_params,OptionHeader,OptionLine,Sub_File,Exception=['',''], Log=False, GPU=False):
