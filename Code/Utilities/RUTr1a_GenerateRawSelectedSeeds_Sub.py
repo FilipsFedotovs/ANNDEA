6f862c2fd7710a2222f1e6cebfc2c4e1e24fe260
Filips <fedotovs@hotmail.com>
2022-10-26 15:15:19 +0100

    Issue #9 - Developing the Track Merger reconstruction bit

--- Code/Utilities/RUTr1a_GenerateRawSelectedSeeds_Sub.py
+++ Code/Utilities/RUTr1a_GenerateRawSelectedSeeds_Sub.py
@@ -110,8 +110,11 @@ r_data.drop(['z'],axis=1,inplace=True)
 data.drop(['e_y'],axis=1,inplace=True)
 data.drop(['e_x'],axis=1,inplace=True)
 data.drop(['e_z'],axis=1,inplace=True)
+print(data)
+exit()
 data.drop(data.index[data['z'] <= PlateZ], inplace = True)
 data=data.rename(columns={"Rec_Seg_ID": "Segment_1"})
+
 data['join_key'] = 'join_key'
 r_data['join_key'] = 'join_key'
 
