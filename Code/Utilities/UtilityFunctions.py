8aaf1df5e7f6f8592c7f8779642aba86ac2b0bb9
Filips <fedotovs@hotmail.com>
2022-10-26 20:21:48 +0100

    Issue #9 - Developing the Track Merger reconstruction bit

--- Code/Utilities/UtilityFunctions.py
+++ Code/Utilities/UtilityFunctions.py
@@ -1082,10 +1082,7 @@ class EMO:
              import torch
              graph = self.GraphSeed
              graph.batch = torch.zeros(len(graph.x),dtype=torch.int64)
-             print(M(graph.x, graph.edge_index, graph.edge_attr,graph.batch))
              self.Fit=M(graph.x, graph.edge_index, graph.edge_attr,graph.batch)[0][1].item()
-             print(self.Fit)
-             exit()
           return self.Fit>=0.5
       @staticmethod
       def unit_vector(vector):
