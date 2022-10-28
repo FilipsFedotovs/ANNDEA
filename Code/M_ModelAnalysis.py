572be53acf63cd2cea981df6da3687e2b7d8b480
Filips <fedotovs@hotmail.com>
2022-10-28 15:41:56 +0100

    Adding Model Analysis

--- Code/M_ModelAnalysis.py
+++ Code/M_ModelAnalysis.py
@@ -22,16 +22,18 @@ parser = argparse.ArgumentParser(description='select cut parameters')
 parser.add_argument('--ModelParams',help="Please enter the train params: '[<Session No>, <Learning Rate>, <Batch size>, <Epochs>]'", default='[1, 0.0001, 4, 10]')
 parser.add_argument('--ModelType',help="What Neural Network type would you like to use: CNN/GNN?", default='CNN')
 parser.add_argument('--ModelArchitecture',help="What Type of Image/Graph: CNN, CNN-E", default='CNN')
+parser.add_argument('--ModelFramework',help="What Type of Image/Graph: CNN, CNN-E", default='Tensorflow')
 ########################################     Initialising Variables    #########################################
 args = parser.parse_args()
 ModelParams=ast.literal_eval(args.ModelParams)
 ModelType=args.ModelType
 ModelArchitecture=args.ModelArchitecture
+ModelFramework=args.ModelFramework
 
 ##################################   Loading Directory locations   ##################################################
 
 import sys
-sys.path.insert(1, AFS_DIR+'/Code/Utilities/')
+sys.path.insert(1, './Utilities/')
 import UtilityFunctions as UF
 
 
@@ -50,7 +52,7 @@ def zero_divide(a, b):
 Meta=UF.TrainingSampleMeta('N/A')
 Meta.IniTrackSeedMetaData(0,0,0,0,[],0,[],0)
 ModelMeta=UF.ModelMeta('N/A')
-ModelMeta.IniModelMeta(ModelParams, 'PyTorch', Meta, ModelArchitecture, 'GNN')
+ModelMeta.IniModelMeta(ModelParams,ModelFramework, Meta, ModelArchitecture, ModelType)
 if ModelType=='CNN':
         import os
         os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
