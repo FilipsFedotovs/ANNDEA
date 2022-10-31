ef84d0c4ca65281a3dc0cb017dd0c6e17112745a
Filips <fedotovs@hotmail.com>
2022-10-31 19:20:31 +0000

    Working on the ID module

--- Code/Utilities/MCTr1a_GenerateRawTrackSamples_Sub.py
+++ Code/Utilities/MCTr1a_GenerateRawTrackSamples_Sub.py
@@ -61,121 +61,61 @@ print(UF.TimeStamp(), "Modules Have been imported successfully...")
 print(UF.TimeStamp(),'Loading pre-selected data from ',input_file_location)
 data=pd.read_csv(input_file_location,header=0,
                     usecols=['x','y','z','tx','ty','Rec_Seg_ID']+ExtraColumns)
-print(data)
+
+track_headers = data[['Rec_Seg_ID']+ExtraColumns]
+track_headers = track_headers.drop_duplicates()
+print(track_headers)
+
+track_data = data[['x','y','z','tx','ty','Rec_Seg_ID']].values.tolist() #Convirting the result to List data type
+print(track_data)
 exit()
-
-print(UF.TimeStamp(),'Creating segment combinations... ')
-data_header = data.groupby('Rec_Seg_ID')['z'].min()  #Keeping only starting hits for the each track record (we do not require the full information about track in this script)
-data_header=data_header.reset_index()
-
-data_end_header = data.groupby('Rec_Seg_ID')['z'].max()  #Keeping only ending hits for the each track record (we do not require the full information about track in this script)
-data_end_header=data_end_header.reset_index()
-data_end_header=data_end_header.rename(columns={"z": "e_z"})
-data_header=pd.merge(data_header, data_end_header, how="inner", on=["Rec_Seg_ID"]) #Shrinking the Track data so just a star hit for each track is present.
-#Doing a plate region cut for the Main Data
-#data_header.drop(data_header.index[data_header['e_z'] > (PlateZ+MaxSLG)], inplace = True) #Not applicable for TSU
-data_header.drop(data_header.index[data_header['z'] < PlateZ], inplace = True)
-Records=len(data_header.axes[0])
-print(UF.TimeStamp(),'There are total of ', Records, 'tracks in the data set')
-
-Cut=math.ceil(MaxRecords/Records) #Even if use only a max of 20000 track on the right join we cannot perform the full outer join due to the memory limitations, we do it in a small 'cuts'
-Steps=math.ceil(MaxSegments/Cut)  #Calculating number of cuts
-data_s=pd.merge(data, data_header, how="inner", on=["Rec_Seg_ID","z"]) #Shrinking the Track data so just a star hit for each track is present.
-data_s.drop(['e_z'],axis=1,inplace=True)
-data_e=pd.merge(data, data_header, how="inner", left_on=["Rec_Seg_ID","z"], right_on=["Rec_Seg_ID","e_z"]) #Shrinking the Track data so just a star hit for each track is present.
-data_e=data_e.rename(columns={"x": "e_x"})
-data_e=data_e.rename(columns={"y": "e_y"})
-data_e.drop(['z_x'],axis=1,inplace=True)
-data_e.drop(['z_y'],axis=1,inplace=True)
-data=pd.merge(data_s, data_e, how="inner", on=["Rec_Seg_ID",'MC_Mother_Track_ID']) #Combining datasets so for each track we know its starting and ending coordinates
-del data_e
-del data_s
-gc.collect()
-
-#What section of data will we cut?
-StartDataCut=j*MaxSegments
-EndDataCut=(j+1)*MaxSegments
-
-#Specifying the right join
-
-r_data=data.rename(columns={"Rec_Seg_ID": "Segment_2"})
-r_data=r_data.rename(columns={'MC_Mother_Track_ID': "Mother_2"})
-r_data.drop(r_data.index[r_data['z'] != PlateZ], inplace = True)
-
-Records=len(r_data.axes[0])
-print(UF.TimeStamp(),'There are  ', Records, 'segments in the starting plate')
-
-r_data=r_data.iloc[StartDataCut:min(EndDataCut,Records)]
-
-
-Records=len(r_data.axes[0])
-print(UF.TimeStamp(),'However we will only attempt  ', Records, 'track segments in the starting plate')
-r_data.drop(['y'],axis=1,inplace=True)
-r_data.drop(['x'],axis=1,inplace=True)
-r_data.drop(['z'],axis=1,inplace=True)
-data.drop(['e_y'],axis=1,inplace=True)
-data.drop(['e_x'],axis=1,inplace=True)
-data.drop(['e_z'],axis=1,inplace=True)
-data.drop(data.index[data['z'] <= PlateZ], inplace = True)
-data=data.rename(columns={"Rec_Seg_ID": "Segment_1"})
-data=data.rename(columns={'MC_Mother_Track_ID': "Mother_1"})
-data['join_key'] = 'join_key'
-r_data['join_key'] = 'join_key'
-
-result_list=[]  #We will keep the result in list rather then Panda Dataframe to save memory
-
-#Downcasting Panda Data frame data types in order to save memory
-data["x"] = pd.to_numeric(data["x"],downcast='float')
-data["y"] = pd.to_numeric(data["y"],downcast='float')
-data["z"] = pd.to_numeric(data["z"],downcast='float')
-
-
-r_data["e_x"] = pd.to_numeric(r_data["e_x"],downcast='float')
-r_data["e_y"] = pd.to_numeric(r_data["e_y"],downcast='float')
-r_data["e_z"] = pd.to_numeric(r_data["e_z"],downcast='float')
-
-#Cleaning memory
-del data_header
-gc.collect()
-
-#Creating csv file for the results
-UF.LogOperations(output_file_location,'w',result_list)
-#This is where we start
-
-for i in range(0,Steps):
-  r_temp_data=r_data.iloc[0:min(Cut,len(r_data.axes[0]))] #Taking a small slice of the data
-  r_data.drop(r_data.index[0:min(Cut,len(r_data.axes[0]))],inplace=True) #Shrinking the right join dataframe
-  merged_data=pd.merge(data, r_temp_data, how="inner", on=['join_key']) #Merging Tracks to check whether they could form a seed
-  merged_data['SLG']=merged_data['z']-merged_data['e_z'] #Calculating the Euclidean distance between Track start hits
-  merged_data['STG']=np.sqrt((merged_data['x']-merged_data['e_x'])**2+((merged_data['y']-merged_data['e_y'])**2)) #Calculating the Euclidean distance between Track start hits
-  merged_data['DynamicCut']=MaxSTG+(merged_data['SLG']*0.96)
-  merged_data.drop(merged_data.index[merged_data['SLG'] < 0], inplace = True) #Dropping the Seeds that are too far apart
-  merged_data.drop(merged_data.index[merged_data['SLG'] > MaxSLG], inplace = True) #Dropping the track segment combinations where the length of the gap between segments is too large
-  merged_data.drop(merged_data.index[merged_data['STG'] > merged_data['DynamicCut']], inplace = True)
-  merged_data.drop(['y','z','x','e_x','e_y','e_z','join_key','STG','SLG','DynamicCut'],axis=1,inplace=True) #Removing the information that we don't need anymore
-  if merged_data.empty==False:
-    merged_data.drop(merged_data.index[merged_data['Segment_1'] == merged_data['Segment_2']], inplace = True) #Removing the cases where Seed tracks are the same
-    merged_data['Seed_Type']=True
-    if len(VetoMotherTrack)>=1:
-      for n in VetoMotherTrack:
-        merged_data['Seed_Type']=((merged_data['Mother_1']==merged_data['Mother_2']) & (merged_data['Mother_1'].str.contains(str('-'+n))==False) & (merged_data['Seed_Type']==True))
-    else:
-        merged_data['Seed_Type']=(merged_data['Mother_1']==merged_data['Mother_2'])
-    merged_data.drop(['Mother_1'],axis=1,inplace=True)
-    merged_data.drop(['Mother_2'],axis=1,inplace=True)
-    merged_list = merged_data.values.tolist() #Convirting the result to List data type
-    result_list+=merged_list #Adding the result to the list
-  if len(result_list)>=2000000: #Once the list gets too big we dump the results into csv to save memory
-      UF.LogOperations(output_file_location,'a',result_list) #Write to the csv
-      #Clearing the memory
-      del result_list
-      result_list=[]
-      gc.collect()
-
-
-UF.LogOperations(output_file_location,'a',result_list) #Writing the remaining data into the csv
-UF.LogOperations(output_result_location,'w',[])
-print(UF.TimeStamp(), "Train seed generation is finished...")
+# tracks = tracks[int(Set)*MaxSegmentsPerJob : min((int(Set)+1)*MaxSegmentsPerJob, len(tracks))]
+# gc.collect()
+#
+# track_counter=0
+# print(UF.TimeStamp(),bcolors.OKGREEN+'Data has been successfully loaded and prepared..'+bcolors.ENDC)
+# #create seeds
+# GoodTracks=[]
+# print(UF.TimeStamp(),'Beginning the image generation part...')
+# limit = len(tracks)
+#
+# for s in range(0,limit):
+#     track=tracks.pop(0)
+#
+#
+#     #label=(track[1] in MotherPDGList)
+#     # for test
+#     #print(track[0], track[1], label)
+#
+#
+#     # 0 for incoming muons, 1 for photons and muons, 2 for others
+#     if track[1] == 0:
+#         label = 0
+#     elif track[1] in MotherPDGList:
+#         label = 1
+#     else :
+#         label = 2
+#
+#     track=Track([track[0]])
+#     track.MCtruthClassifyTrack(label)
+#
+#
+#
+#     track.DecorateSegments(segments)
+#     GoodTracks.append(track)
+#     del track
+#     continue
+#
+# print('The raw image generation has been completed..')
+# del tracks
+# del segments
+# gc.collect()
+# print(UF.TimeStamp(),'Saving the results..')
+#
+#
+#
+# UF.LogOperations(output_file_location,'w',GoodTracks) #Writing the remaining data into the csv
+# print(UF.TimeStamp(), "Train seed generation is finished...")
 #End of the script
 
 
