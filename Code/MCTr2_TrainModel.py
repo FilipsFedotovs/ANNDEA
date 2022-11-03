777169579268ea22b5fd60b83b9b1ed05bc7485b
Filips <fedotovs@hotmail.com>
2022-11-03 20:03:02 +0000

    Working on the ID module

--- Code/MCTr2_TrainModel.py
+++ Code/MCTr2_TrainModel.py
@@ -159,6 +159,7 @@ if Mode=='RESET':
  MetaInput=UF.PickleOperations(TrainSampleInputMeta,'r', 'N/A')
  print(MetaInput[1])
  Meta=MetaInput[0]
+ print(ModelParams)
  print(Meta.ClassHeaders)
  exit()
  Model_Meta_Path=EOSsubModelDIR+'/'+args.ModelName+'_Meta'
