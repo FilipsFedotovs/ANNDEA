c40dec7b4108c4cd5e719316ca0c04e24181ff2e
Filips <fedotovs@hotmail.com>
2022-11-07 21:39:30 +0000

    Creating the SND data preparation script

--- Code/PrepareData.py
+++ Code/PrepareData.py
@@ -60,6 +60,8 @@ print(UF.TimeStamp(),bcolors.BOLD+'Stage 1:'+bcolors.ENDC+' Preparing the input 
 ######################################## Set variables  #############################################################
 
 def MotherIDNorm(row):
+        print(row)
+        exit()
         if row['Z']>=row['Fiducial_Cut_z_LB'] and row['Z']<=row['Fiducial_Cut_z_UB']:
           return row['MCMotherID']
         else:
