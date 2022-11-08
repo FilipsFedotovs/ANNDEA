8be0acea53609592c1a5d561f2f8dbbb2cbdd301
Filips <fedotovs@hotmail.com>
2022-11-08 20:37:07 +0000

    Testing the Tracking Module

--- Code/Utilities/MTr1_GenerateTrainClusters_Sub.py
+++ Code/Utilities/MTr1_GenerateTrainClusters_Sub.py
@@ -73,17 +73,12 @@ data["Hit_ID"] = data["Hit_ID"].astype(str)
 data['z']=data['z']-z_offset
 x_max=data['x'].max()
 y_max=data['y'].max()
-print(data)
-print((X_ID+1)*stepX)
-x=input()
 print(UF.TimeStamp(),'Creating clusters... ')
 data.drop(data.index[data['z'] >= ((Z_ID+1)*stepZ)], inplace = True)  #Keeping the relevant z slice
 data.drop(data.index[data['z'] < (Z_ID*stepZ)], inplace = True)  #Keeping the relevant z slice
 
 data.drop(data.index[data['x'] >= ((X_ID+1)*stepX)], inplace = True)  #Keeping the relevant z slice
 data.drop(data.index[data['x'] < (X_ID*stepX)], inplace = True)  #Keeping the relevant z slice
-print(data)
-exit()
 data_list=data.values.tolist()
 
 if Y_overlap==1:
