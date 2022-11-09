97080a3d2855e223db80bff08c222295f367c6ca
Filips <fedotovs@hotmail.com>
2022-11-09 23:00:45 +0000

    Testing the Tracking Module

--- Code/MTr1_GenerateTrainClusters.py
+++ Code/MTr1_GenerateTrainClusters.py
@@ -84,7 +84,7 @@ if os.path.isfile(destination_output_file_location) and Mode!='RESET':
     ValSamples=[]
     TestSamples=[]
 #            for i in range(1,Meta.no_sets+1):
-    for i in range(1,100):
+    for i in range(1,Meta.no_sets+1):
         flocation=EOS_DIR+'/ANNDEA/Data/TRAIN_SET/'+TrainSampleID+'_TTr_OUTPUT_'+str(i)+'.pkl'
         print(UF.TimeStamp(),'Loading data from ',bcolors.OKBLUE+flocation+bcolors.ENDC)
         TrainClusters=UF.PickleOperations(flocation,'r', 'N/A')
