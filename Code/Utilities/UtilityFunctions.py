a52cebef4098a5dca7163c08db8d1c5085b2ce28
Filips <fedotovs@hotmail.com>
2022-11-15 22:10:16 +0000

    Optimisingthe tracker submission file

--- Code/Utilities/UtilityFunctions.py
+++ Code/Utilities/UtilityFunctions.py
@@ -2241,7 +2241,7 @@ def SubmitJobs2Condor(job,local=False):
         TotalLine = 'python3 ' + job[5] + OptionLine
         TotalLine = 'python3 ' + job[5] + OptionLine
         print(job[5][:-1])
-        subprocess.call(['python3',job[5][:-1],OptionLine[1:]])
+        subprocess.call(['python3',job[5][:-1],OptionLine],Shell=True)
         print(TotalLine, " has been successfully executed")
     else:
         SHName = job[2]
