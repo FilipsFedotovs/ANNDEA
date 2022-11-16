95428c59def2c1fc21a9d5711d017aeed660da44
Filips <fedotovs@hotmail.com>
2022-11-16 10:20:54 +0000

    Optimisingthe tracker submission file

--- Code/Utilities/UtilityFunctions.py
+++ Code/Utilities/UtilityFunctions.py
@@ -2240,7 +2240,8 @@ def SubmitJobs2Condor(job,local=False):
                 OptionLine+=str(job[1][line])
                 TotalLine = 'python3 ' + job[5] + OptionLine
        submission_line='python3 '+job[5][:-1]+OptionLine
-
+       print(job[6])
+       exit()
        for j in range(job[6]):
          submission_line=submission_line.replace('$1',str(j))
          subprocess.call([submission_line],shell=True)
