09b9d3b7090ff60ef6b1664f778d2b664ec6f22e
Filips <fedotovs@hotmail.com>
2022-11-26 14:57:31 +0000

    Changing submission scripts so they point to a different PyLocation. Motivated by heavy loads on HTCondor

--- Code/Utilities/UtilityFunctions.py
+++ Code/Utilities/UtilityFunctions.py
@@ -3,6 +3,7 @@ import sys
 
 sys.path.append('/usr/lib64/python36.zip')
 sys.path.append('/usr/lib64/python3.6')
+sys.path.append('/usr/lib64/python3.6/lib-dynload')
 print(sys.path)
 import csv
 exit()
