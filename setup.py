b11582633d1aba51018d21f8b9bd001b5010470c
Filips <fedotovs@hotmail.com>
2022-11-26 14:35:39 +0000

    Updating setup file in order to point py scripts to the different folders for the import. Motivated by heavy loads on HTCondor

--- setup.py
+++ setup.py
@@ -117,6 +117,7 @@ string_to_write.append('EOS_DIR')
 string_to_write.append(EOSDir)
 dir_writer.writerow(string_to_write)
 print(bcolors.OKGREEN + 'Updated the directory mapping file with EOS location' + bcolors.ENDC)
+string_to_write = []
 string_to_write.append('PY_DIR')
 string_to_write.append(PyPath)
 dir_writer.writerow(string_to_write)
