2aca8b8692de95bd0325fe238323e2419707493c
Filips <fedotovs@hotmail.com>
2022-12-08 16:57:28 +0500

    Final code simplification for TRr1

--- Code/RTr1_ReconstructTracks.py
+++ Code/RTr1_ReconstructTracks.py
@@ -4,7 +4,26 @@
 
 
 ########################################    Import libraries    #############################################
-
+import csv
+csv_reader=open('../config',"r")
+config = list(csv.reader(csv_reader))
+for c in config:
+    if c[0]=='AFS_DIR':
+        AFS_DIR=c[1]
+    if c[0]=='EOS_DIR':
+        EOS_DIR=c[1]
+    if c[0]=='PY_DIR':
+        PY_DIR=c[1]
+csv_reader.close()
+import sys
+if PY_DIR!='': #Temp solution
+    sys.path=['',PY_DIR]
+    sys.path.append('/usr/lib64/python36.zip')
+    sys.path.append('/usr/lib64/python3.6')
+    sys.path.append('/usr/lib64/python3.6/lib-dynload')
+    sys.path.append('/usr/lib64/python3.6/site-packages')
+    sys.path.append('/usr/lib/python3.6/site-packages')
+sys.path.append(AFS_DIR+'/Code/Utilities')
 import pandas as pd #We use Panda for a routine data processing
 import math #We use it for data manipulation
 import numpy as np
