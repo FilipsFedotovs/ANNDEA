fa0f5da1137f0a6bccfa84d9f2ba9502d7b70674
Filips Fedotovs <74325502+FilipsFedotovs@users.noreply.github.com>
2022-12-28 10:11:54 +0000

    Update ETr_EvalTrackRec.py

--- Code/ETr_EvalTrackRec.py
+++ Code/ETr_EvalTrackRec.py
@@ -66,7 +66,8 @@ input_file_location=args.f
 SkipRcmb=args.SkipRcmb=='N'
 Xmin,Xmax,Ymin,Ymax=float(args.Xmin),float(args.Xmax),float(args.Ymin),float(args.Ymax)
 SliceData=max(Xmin,Xmax,Ymin,Ymax)>0 #We don't slice data if all values are set to zero simultaneousy (which is the default setting)
-print(args.f.rfind('/'))
+se=(args.f.rfind('/'))
+print(args.f[se:-3])
 exit()
 print(UF.TimeStamp(), 'Loading ',bcolors.OKBLUE+input_file_location+bcolors.ENDC)
 columns_to_extract=[PM.x,PM.y,PM.z,PM.Hit_ID,PM.MC_Event_ID,PM.MC_Track_ID]
