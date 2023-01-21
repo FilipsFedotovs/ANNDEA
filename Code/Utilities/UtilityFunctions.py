bcaa799419e9a11ba821be9a238fc11c601e6640
Filips <fedotovs@hotmail.com>
2023-01-21 21:09:23 +0000

    Simplifying merger Train File generator

--- Code/Utilities/UtilityFunctions.py
+++ Code/Utilities/UtilityFunctions.py
@@ -2340,7 +2340,7 @@ def ManageTempFolders(spi,op_type):
        _tot=spi[1][8]
     else:
        _tot=len(spi[1][8])
-    print(_tot)
+    print(spi)
     x=input()
     if op_type=='Create':
        for i in range(_tot):
