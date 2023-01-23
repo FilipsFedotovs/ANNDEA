7befb84b461d7baf9d7acecf64e532699d49aa4c
Filips <fedotovs@hotmail.com>
2023-01-23 23:06:17 +0000

    Simplifying merger Train File generator

--- Code/Utilities/RTr1a_ReconstructTracks_Sub.py
+++ Code/Utilities/RTr1a_ReconstructTracks_Sub.py
@@ -148,7 +148,7 @@ for k in range(0,Z_ID_Max):
     temp_data_list=temp_data.values.tolist()
     print(temp_data)
     continue
-    
+
     if Log!='NO':
        temp_MCData=MCdata.drop(MCdata.index[MCdata['z'] >= ((Z_ID+1)*stepZ)])  #Keeping the relevant z slice
        temp_MCData=temp_MCData.drop(temp_MCData.index[temp_MCData['z'] < (Z_ID*stepZ)])  #Keeping the relevant z slice
