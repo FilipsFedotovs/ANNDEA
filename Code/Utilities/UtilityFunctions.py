26afa262941f27a607587259204f4dc0a5b251ec
Filips <fedotovs@hotmail.com>
2023-02-08 22:14:18 +0000

    Temp - optimisation studies

--- Code/Utilities/UtilityFunctions.py
+++ Code/Utilities/UtilityFunctions.py
@@ -249,13 +249,14 @@ class HitCluster:
            _Tot_Hits.drop(_Tot_Hits.index[_Tot_Hits['l_HitID'] == _Tot_Hits['r_HitID']], inplace = True)
 
            _Tot_Hits.drop(_Tot_Hits.index[_Tot_Hits['l_z'] <= _Tot_Hits['r_z']], inplace = True)
-           print(_Tot_Hits)
+
            _Tot_Hits['d_tx'] = _Tot_Hits['l_tx']-_Tot_Hits['r_tx']
            _Tot_Hits['d_tx'] = _Tot_Hits['d_tx'].abs()
            _Tot_Hits['d_ty'] = _Tot_Hits['l_ty']-_Tot_Hits['r_ty']
            _Tot_Hits['d_ty'] = _Tot_Hits['d_ty'].abs()
            _Tot_Hits.drop(_Tot_Hits.index[_Tot_Hits['d_tx'] >= cut_dt], inplace = True)
            _Tot_Hits.drop(_Tot_Hits.index[_Tot_Hits['d_ty'] >= cut_dt], inplace = True)
+           print(_Tot_Hits)
            _Tot_Hits['d_x'] = (_Tot_Hits['r_x']-(_Tot_Hits['l_x']+(_Tot_Hits['l_tx']*(_Tot_Hits['r_z']-_Tot_Hits['l_z']))))
            _Tot_Hits['d_x'] = _Tot_Hits['d_x'].abs()
            _Tot_Hits['d_y'] = (_Tot_Hits['r_y']-(_Tot_Hits['l_y']+(_Tot_Hits['l_ty']*(_Tot_Hits['r_z']-_Tot_Hits['l_z']))))
@@ -718,11 +719,21 @@ class HitCluster:
               _Bottom.append(_ClusterID.index(ip[1]))
           return [_Top,_Bottom]
 
-      def JoinHits(_H1,_H2):
+      def JoinHits(_H1,_H2, _cdt):
           if _H1[0]==_H2[0]:
               return False
           elif _H1[3]<=_H2[3]:
               return False
+          else:
+              _dtx=abs(_H1[4]-_H2[4])
+              if _dtx>=_cdt:
+                  return False
+              else:
+                  _dty=abs(_H1[5]-_H2[5])
+                  if _dty>=_cdt:
+                      return False
+
+
           return True
       def GenerateEdgeAttributes(_input):
           _EdgeAttr=[]
