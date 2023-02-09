8a969a3c3882845c1e4a0547f300245f31092167
Filips <fedotovs@hotmail.com>
2023-02-09 23:03:25 +0000

    Temp - optimisation studies

--- Code/Utilities/UtilityFunctions.py
+++ Code/Utilities/UtilityFunctions.py
@@ -535,10 +535,17 @@ class HitCluster:
                 _Tot_Hits_PCopy=copy.deepcopy(_Tot_Hits)
                 _Tot_Hits_Predator=[]
 
-                for Predator in _Tot_Hits_PCopy:
-                    for Prey in _Tot_Hits_PCopy:
-                          if Predator!=Prey:
-                           Predator=HitCluster.InjectHit(Predator,Prey,False)[0]
+                # for Predator in _Tot_Hits_PCopy:
+                #     for Prey in _Tot_Hits_PCopy:
+                #           if Predator!=Prey:
+                #            Predator=HitCluster.InjectHit(Predator,Prey,False)[0]
+                #            print(Predator)
+                #            exit()
+                #     _Tot_Hits_Predator.append(Predator)
+
+                for prd in range(0,len(_Tot_Hits_PCopy)):
+                    for pry in range(prd+1,len(_Tot_Hits_PCopy)):
+                           Predator=HitCluster.InjectHit(_Tot_Hits_PCopy[prd],_Tot_Hits_PCopy[pry],False)[0]
                            print(Predator)
                            exit()
                     _Tot_Hits_Predator.append(Predator)
