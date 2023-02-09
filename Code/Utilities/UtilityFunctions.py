5d12187c0e189394566dcfacdb82852df34ff0b3
Filips <fedotovs@hotmail.com>
2023-02-09 23:13:09 +0000

    Temp - optimisation studies

--- Code/Utilities/UtilityFunctions.py
+++ Code/Utilities/UtilityFunctions.py
@@ -544,8 +544,9 @@ class HitCluster:
                 #     _Tot_Hits_Predator.append(Predator)
 
                 for prd in range(0,len(_Tot_Hits_PCopy)):
+                    Predator=_Tot_Hits_PCopy[prd]
                     for pry in range(prd+1,len(_Tot_Hits_PCopy)):
-                           Predator=_Tot_Hits_PCopy[prd]
+                           
                            Predator=HitCluster.InjectHit(Predator,_Tot_Hits_PCopy[pry],False)[0]
                            print(_Tot_Hits_PCopy[0])
                            print(Predator)
