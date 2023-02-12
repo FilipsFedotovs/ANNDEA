06651487507d31996c34ec8237fddef613c71a9a
Filips <fedotovs@hotmail.com>
2023-02-12 14:50:20 +0000

    Temp - optimisation studies

--- Code/RTr1_ReconstructTracks.py
+++ Code/RTr1_ReconstructTracks.py
@@ -474,6 +474,8 @@ while Status<len(Program):
            FirstFile=EOS_DIR+'/ANNDEA/Data/REC_SET/Temp_RTr1c_'+RecBatchID+'_0'+'/RTr1c_'+RecBatchID+'_hit_cluster_rec_x_set_0.csv'
            print(UF.TimeStamp(),'Loading the file ',bcolors.OKBLUE+FirstFile+bcolors.ENDC)
            TrackMap=pd.read_csv(FirstFile,header=0)
+           print(TrackMap)
+           exit()
            input_file_location=args.f
            print(UF.TimeStamp(),'Loading raw data from',bcolors.OKBLUE+input_file_location+bcolors.ENDC)
            #Reading the original file with Raw hits
