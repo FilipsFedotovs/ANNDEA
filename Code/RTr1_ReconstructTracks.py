7e7ac2f836e85115409fe899424bb7fa808c6da3
Filips <fedotovs@hotmail.com>
2023-02-13 18:20:09 +0000

    Temp - optimisation studies

--- Code/RTr1_ReconstructTracks.py
+++ Code/RTr1_ReconstructTracks.py
@@ -145,7 +145,7 @@ if os.path.isfile(required_file_location)==False or Mode=='RESET':
          print(UF.TimeStamp(),'The cleaned data has ',final_rows,' hits')
          data[PM.Hit_ID] = data[PM.Hit_ID].astype(int)
 
-         data=data.drop(data.index[(PM.Hit_ID > 27838915) | (data[PM.Hit_ID] < 27838860)])
+         data=data.drop(data.index[(data[PM.Hit_ID] > 27838915) | (data[PM.Hit_ID] < 27838860)])
          print(data)
          data[PM.Hit_ID] = data[PM.Hit_ID].astype(str)
          if SliceData:
