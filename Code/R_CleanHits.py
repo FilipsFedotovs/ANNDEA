e6ead15805916febcf09e399822bcda7e8a34292
Filips <fedotovs@hotmail.com>
2023-02-23 20:54:45 +0000

    Adding the deduplicator

--- Code/R_CleanHits.py
+++ Code/R_CleanHits.py
@@ -74,8 +74,11 @@ New_Data['Rec_Seg_ID'] = New_Data[RecBatchID+'_Brick_ID'] + '-' + New_Data[RecBa
 New_Data.drop_duplicates(subset=[RecBatchID+'_Brick_ID',RecBatchID+'_Track_ID',PM.z],keep='first',inplace=True)
 compress_data=New_Data.drop([RecBatchID+'_Brick_ID',RecBatchID+'_Track_ID',PM.z],axis=1)
 compress_data['Track_No']= compress_data[PM.Hit_ID]
-compress_data=compress_data.groupby(by=['Rec_Seg_ID'])['Track_No'].count().reset_index()
-print(compress_data)
+compress_data=compress_data.groupby(by=['Rec_Seg_ID'])['Hit_No'].count().reset_index()
+New_Data=pd.merge(New_Data, compress_data, how="left", on=['Rec_Seg_ID'])
+New_Data = New_Data[New_Data.Hit_No >= PM.MinHitsTrack]
+Data=pd.merge(Data,New_Data,how='left', on=[PM.Hit_ID])
+print(Data)
 exit()
            #
            # CutData.drop([RecBatchID+'_Brick_ID',RecBatchID+'_Track_ID'],axis=1,inplace=True,errors='ignore') #Removing old ANNDEA reconstruction results so we can overwrite with the new ones
