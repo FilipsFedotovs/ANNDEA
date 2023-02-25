90fb74d0b45dea916c17a83e1b4cb96937c42fbb
Filips <fedotovs@hotmail.com>
2023-02-25 16:35:08 +0000

    Adding the deduplicator

--- Code/RTr1_ReconstructTracks.py
+++ Code/RTr1_ReconstructTracks.py
@@ -478,7 +478,8 @@ while Status<len(Program):
            Good_Tracks=Hit_Map[Hit_Map.No_Plates == Hit_Map.No_Hits] #For all good tracks the number of hits matches the number of plates, we won't touch them
            Good_Tracks=Good_Tracks[[RecBatchID+'_Brick_ID',RecBatchID+'_Track_ID',PM.Hit_ID]] #Just strip off the information that we don't need anymore
            print(Good_Tracks)
-           Bad_Tracks=Hit_Map[Hit_Map.No_Plates > Hit_Map.No_Hits] #These are the bad guys. We need to remove this extra hits
+           Bad_Tracks=Hit_Map[Hit_Map.No_Plates < Hit_Map.No_Hits] #These are the bad guys. We need to remove this extra hits
+
            print(Bad_Tracks)
            exit()
 
