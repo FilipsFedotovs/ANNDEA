49ebb532eacc0bfda8108e19e9145c85a98c7d9f
Filips <fedotovs@hotmail.com>
2023-02-27 23:07:30 +0000

    Adding the deduplicator

--- Code/R_CleanHits.py
+++ Code/R_CleanHits.py
@@ -213,13 +213,11 @@ print(UF.TimeStamp(),'Removing problematic hits...')
 Bad_Tracks=pd.merge(Bad_Tracks,Bad_Tracks_Head,how='inner',on = [RecBatchID+'_Brick_ID',RecBatchID+'_Track_ID'])
 
 
-print(Bad_Tracks.dtypes)
-
 print(UF.TimeStamp(),'Calculating x and y coordinates of the fitted line for all plates in the track...')
 #Calculating x and y coordinates of the fitted line for all plates in the track
 Bad_Tracks['new_x']=Bad_Tracks['ax']+(Bad_Tracks[PM.z]*Bad_Tracks['t1x'])+((Bad_Tracks[PM.z]**2)*Bad_Tracks['t2x'])
 Bad_Tracks['new_y']=Bad_Tracks['ay']+(Bad_Tracks[PM.z]*Bad_Tracks['t1y'])+((Bad_Tracks[PM.z]**2)*Bad_Tracks['t2y'])
-print(Bad_Tracks)
+
 #Calculating how far hits deviate from the fit polynomial
 print(UF.TimeStamp(),'Calculating how far hits deviate from the fit polynomial...')
 Bad_Tracks['d_x']=Bad_Tracks[PM.x]-Bad_Tracks['new_x']
