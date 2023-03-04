b9daadc38f72e2a0872da439107a330b25d44419
Filips <fedotovs@hotmail.com>
2023-03-04 14:14:58 +0000

    Fix a small bug in the Trainer file

--- Code/Utilities/UtilityFunctions.py
+++ Code/Utilities/UtilityFunctions.py
@@ -152,8 +152,6 @@ class ModelMeta:
                     for i in test_input:
                                LossDataForChecking.append(i[6])
                                AccDataForChecking.append(i[7])
-                    LossDataForChecking=[1,1,1]
-                    AccDataForChecking=[1,2,3]
                     LossGrad=GetEquationOfLine(LossDataForChecking)[0]
                     AccGrad=GetEquationOfLine(AccDataForChecking)[0]
                     print(LossGrad,AccGrad)
