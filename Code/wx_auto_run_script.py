3bd33c4e48b5a421cb3301e3d3ca948937781645
WenqingXie47 <wenqing_xie@126.com>
2023-03-04 20:43:28 +0000

    auto script

--- Code/wx_auto_run_script.py
+++ Code/wx_auto_run_script.py
@@ -6,10 +6,10 @@ script_name = "MUTr2_TrainModel.py"
 
 
 model_parameters_dict={
-    "2T_CNN_ANN_BA_1_1_model_wx": [[1, 4, 1, 20, 2, 2, 2], [], [],[], [], [1, 4, 2], [], [], [], [], [7,2],[5000,5000,3000,50]],
-    "2T_CNN_ANN_BA_2_1_model_wx": [[1, 4, 1, 20, 2, 2, 2], [1,4,1,20,2,2,2], [],[], [], [1, 4, 2], [], [], [], [], [7,2],[5000,5000,3000,50]],
-    "2T_CNN_ANN_BA_1_2_model_wx": [[1, 4, 1, 20, 2, 2, 2], [], [],[], [], [1, 4, 2], [1,4,2], [], [], [], [7,2],[5000,5000,3000,50]],
-    "2T_CNN_ANN_BA_2_2_model_wx": [[1, 4, 1, 20, 2, 2, 2], [1,4,1,20,2,2,2], [],[], [], [1, 4, 2], [1,4,2], [], [], [], [7,2],[5000,5000,3000,50]],
+    "2T_CNN_ANN_BA_1_1_model": [[1, 4, 2, 5, 2, 2, 2], [], [],[], [], [1, 4, 2], [], [], [], [], [7,2],[5000,5000,3000,50]],
+    "2T_CNN_ANN_BA_2_1_model": [[1, 4, 2, 5, 2, 2, 2], [1,4,2,5,2,2,2], [],[], [], [1, 4, 2], [], [], [], [], [7,2],[5000,5000,3000,50]],
+    "2T_CNN_ANN_BA_1_2_model": [[1, 4, 2, 5, 2, 2, 2], [], [],[], [], [1, 4, 2], [1,4,2], [], [], [], [7,2],[5000,5000,3000,50]],
+    "2T_CNN_ANN_BA_2_2_model": [[1, 4, 2, 5, 2, 2, 2], [1,4,2,5,2,2,2], [],[], [], [1, 4, 2], [1,4,2], [], [], [], [7,2],[5000,5000,3000,50]],
 }
 
 fixed_option_dict = {
@@ -40,6 +40,6 @@ for model_name, model_parameters in model_parameters_dict.items():
     variable_options = " --ModelName " + model_name +" --ModelParams " + '"'+str(model_parameters)+'"'
     fixed_options = get_fixed_options(fixed_option_dict)
     command = "python3 "+script_name + variable_options + fixed_options
-    subprocess.call(command, shell=True)
+    subprocess.Popen(command, shell=True)
 
     
