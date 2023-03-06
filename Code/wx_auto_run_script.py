0d8e88115600406fd8e9b1a972378d2054053b0a
WenqingXie47 <wenqing_xie@126.com>
2023-03-06 12:08:41 +0000

    auto script

--- Code/wx_auto_run_script.py
+++ Code/wx_auto_run_script.py
@@ -6,10 +6,10 @@ script_name = "MUTr2_TrainModel.py"
 
 
 model_parameters_dict={
-    "2T_CNN_ANN_BA_1_1_model": [[1, 4, 2, 5, 2, 2, 2], [], [],[], [], [1, 4, 2], [], [], [], [], [7,2],[5000,5000,3000,50]],
-    "2T_CNN_ANN_BA_2_1_model": [[1, 4, 2, 5, 2, 2, 2], [1,4,2,5,2,2,2], [],[], [], [1, 4, 2], [], [], [], [], [7,2],[5000,5000,3000,50]],
-    "2T_CNN_ANN_BA_1_2_model": [[1, 4, 2, 5, 2, 2, 2], [], [],[], [], [1, 4, 2], [1,4,2], [], [], [], [7,2],[5000,5000,3000,50]],
-    "2T_CNN_ANN_BA_2_2_model": [[1, 4, 2, 5, 2, 2, 2], [1,4,2,5,2,2,2], [],[], [], [1, 4, 2], [1,4,2], [], [], [], [7,2],[5000,5000,3000,50]],
+    "2T_CNN_ANN_BA_1_1_model": [[1, 4, 1, 3, 2, 2, 2], [], [],[], [], [1, 4, 2], [], [], [], [], [7,2],[5000,5000,3000,50]],
+    "2T_CNN_ANN_BA_2_1_model": [[1, 4, 1, 3, 2, 2, 2], [1,4,1,3,2,2,2], [],[], [], [1, 4, 2], [], [], [], [], [7,2],[5000,5000,3000,50]],
+    "2T_CNN_ANN_BA_1_2_model": [[1, 4, 1, 3, 2, 2, 2], [], [],[], [], [1, 4, 2], [1,4,2], [], [], [], [7,2],[5000,5000,3000,50]],
+    "2T_CNN_ANN_BA_2_2_model": [[1, 4, 1, 3, 2, 2, 2], [1,4,1,3,2,2,2], [],[], [], [1, 4, 2], [1,4,2], [], [], [], [7,2],[5000,5000,3000,50]],
 }
 
 fixed_option_dict = {
@@ -18,12 +18,26 @@ fixed_option_dict = {
     "ModelArchitecture" : "CNN",
 }
 
+model_parameters_dict_gnn={
+    "2T_GNN_FC_16_16_model": [[16],[16],[],[],[],[],[],[],[],[],[7,2],[5000,5000,3000,50]],
+    "2T_GNN_FC_16_16_16_model": [[16],[16],[16],[],[],[],[],[],[],[],[7,2],[5000,5000,3000,50]],
+    "2T_GNN_FC_32_32_model": [[32],[32],[],[],[],[],[],[],[],[],[7,2],[5000,5000,3000,50]],
+    "2T_GNN_FC_32_32_32_model": [[32],[32],[32],[],[],[],[],[],[],[],[7,2],[5000,5000,3000,50]],
+}
+
+
+fixed_option_dict_gnn = {
+    "TrainSampleID": "SND_RTr_MC1_Train_Data_Combined",
+    "ModelType": "GNN",
+    "ModelArchitecture" : "GNN-4N-FC",
+}
+
+
 
 option_headers=[
     "ModelName",
     "ModelParams",
 ]
-
 
 
 
@@ -34,6 +48,11 @@ def get_fixed_options(dict):
         s+= (" " + option_value +" ")
     return s
 
+
+gnn = True
+if(gnn):
+    fixed_option_dict = fixed_option_dict_gnn
+    model_parameters_dict = model_parameters_dict_gnn
 
 
 for model_name, model_parameters in model_parameters_dict.items():
