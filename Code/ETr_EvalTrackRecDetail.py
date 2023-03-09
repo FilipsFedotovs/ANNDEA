2a0736cd4ebd72d9720b3f5ffd3f4bffdd55ea0e
DinisBeleza <124186258+DinisBeleza@users.noreply.github.com>
2023-03-09 13:06:59 +0000

    Update ETr_EvalTrackRecDetail.py

--- Code/ETr_EvalTrackRecDetail.py
+++ Code/ETr_EvalTrackRecDetail.py
@@ -107,7 +107,7 @@ with alive_bar(iterations,force_tty=True, title = 'Calculating densities.') as b
                 
                 if len(ANN_test) > 0:                                       
                     ANN_test = ANN_test.astype({col: 'int8' for col in ANN_test.select_dtypes('int64').columns})
-                    ANN_test = ANN_test.astype({col: 'float8' for col in ANN_test.select_dtypes('float64').columns})
+                    ANN_test = ANN_test.astype({col: 'float16' for col in ANN_test.select_dtypes('float64').columns})
                     print(ANN_test.dtypes)
                     exit()
 
