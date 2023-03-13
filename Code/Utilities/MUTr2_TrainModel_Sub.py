67e0a45cd22acc911e7655e67a93684525f847cc
Filips <fedotovs@hotmail.com>
2023-03-13 23:27:44 +0000

    Fix GMM bug

--- Code/Utilities/MUTr2_TrainModel_Sub.py
+++ Code/Utilities/MUTr2_TrainModel_Sub.py
@@ -188,6 +188,7 @@ elif ModelMeta.ModelType=='GNN':
        import torch_geometric
        from torch_geometric.loader import DataLoader
        print(train_dataset[0].x)
+       print(train_dataset[0].y)
        print(TrainParams[1])
        TrainSamples = DataLoader(train_dataset, batch_size=TrainParams[1], shuffle=True)
        ValSamples = DataLoader(val_dataset, batch_size=TrainParams[1], shuffle=False)
