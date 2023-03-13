634a6a4e9b6ab00706188f0b11395e4d5c4368de
DinisBeleza <124186258+DinisBeleza@users.noreply.github.com>
2023-03-13 14:16:35 +0000

    Update Track_Analysis.py

--- Code/Track_Analysis.py
+++ Code/Track_Analysis.py
@@ -34,8 +34,8 @@ hitdata['ty'] = pd.to_numeric(hitdata['ty'],errors='coerce').fillna(0.00).astype
 #ANN_test = ANN_test.astype({col: 'int8' for col in ANN_test.select_dtypes('int64').columns})
 hitdata=hitdata.groupby(['tx','ty']).Hit_ID.nunique().reset_index()
 sns.heatmap(hitdata, annot=True)
-#plt.show()
-#exit()
+plt.show()
+exit()
 
 # number of segments by specific Track IDs
 angle_columns = ['tx','ty',args.TrackName]
