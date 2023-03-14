947e7d2a0d993dd02a6f51c6ba4a111defd2577e
Filips <fedotovs@hotmail.com>
2023-03-14 20:59:42 +0000

    Fix GMM bug

--- Code/Utilities/UtilityFunctions.py
+++ Code/Utilities/UtilityFunctions.py
@@ -1661,9 +1661,9 @@ def GenerateModel(ModelMeta,TrainParams=None):
                     super(GMM, self).__init__()
                     torch.manual_seed(12345)
                     if len(HiddenLayer)==3:
-                        self.conv1 = GMMConv(5 , HiddenLayer[0][0],dim=3,kernel_size=HiddenLayer[0][1])
-                        self.conv2 = GMMConv(HiddenLayer[0][0],HiddenLayer[1][0],dim=3,kernel_size=HiddenLayer[1][1])
-                        self.conv3 = GMMConv(HiddenLayer[1][0],HiddenLayer[2][0],dim=3,kernel_size=HiddenLayer[2][1])
+                        self.conv1 = GMMConv(5 , HiddenLayer[0][0],dim=5,kernel_size=HiddenLayer[0][1])
+                        self.conv2 = GMMConv(HiddenLayer[0][0],HiddenLayer[1][0],dim=5,kernel_size=HiddenLayer[1][1])
+                        self.conv3 = GMMConv(HiddenLayer[1][0],HiddenLayer[2][0],dim=5,kernel_size=HiddenLayer[2][1])
                         self.lin = Linear(HiddenLayer[2][0],OutputLayer[1])
                     self.softmax = Softmax(dim=-1)
 
