3502d016b88c4d916bdd73675f77b994a08acacb
Filips <fedotovs@hotmail.com>
2023-03-15 22:11:16 +0000

    Add ability to Dinis script to look at reconstruction metrics by Mother PDG

--- Code/ETr_EvalTrackRecDetail.py
+++ Code/ETr_EvalTrackRecDetail.py
@@ -135,13 +135,16 @@ with alive_bar(iterations,force_tty=True, title = 'Calculating densities.') as b
                 
                 
 
-                if len(ANN_test) > 0:          
+                if len(ANN_test) > 20:
+                    print(ANN_test)
                     ANN_test[args.TrackName] = pd.to_numeric(ANN_test[args.TrackName],errors='coerce').fillna(-2).astype('int')
+                    print(ANN_test)
+                    exit()
                     ANN_test['z_coord'] = ANN_test['z_coord'].astype('int')
                     ANN_test = ANN_test.astype({col: 'int8' for col in ANN_test.select_dtypes('int64').columns})
                     #print(ANN_test.dtypes)
                     #exit()
-
+                continue
                 ANN_test_right = ANN_test.rename(columns={'Hit_ID':'Hit_ID_right',args.TrackName:args.TrackName+'_right','MC_Track':'MC_Track_right','z_coord':'z_coord_right','Mother_Group':'Mother_Group_right'})
 
                 ANN_test_all = pd.merge(ANN_test,ANN_test_right,how='inner',on=['x'])
