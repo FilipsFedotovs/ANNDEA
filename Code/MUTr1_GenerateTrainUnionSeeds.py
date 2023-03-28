4b1ac6ad70d866f39f064b6754880c0edc427ab9
Filips Fedotovs <74325502+FilipsFedotovs@users.noreply.github.com>
2023-03-28 11:32:03 +0100

    Update MUTr1_GenerateTrainUnionSeeds.py

--- Code/MUTr1_GenerateTrainUnionSeeds.py
+++ Code/MUTr1_GenerateTrainUnionSeeds.py
@@ -269,9 +269,6 @@ def StandardProcess(program,status,freshstart):
                                     program[status][1][9],
                                     False,
                                     program[status][6])
-        print(program[status][6])
-        print(bad_pop)
-        exit()
         if len(bad_pop)==0:
              print(UF.TimeStamp(),bcolors.OKGREEN+'Stage '+str(status)+' has successfully completed'+bcolors.ENDC)
              return True,False
@@ -392,7 +389,7 @@ elif type(job_sets[0][0]) is int:
                         for lp in job_sets:
                             TotJobs+=np.sum(lp)
 prog_entry.append(' Sending hit cluster to the HTCondor, so tack segment combination pairs can be formed...')
-prog_entry.append([AFS_DIR,EOS_DIR,PY_DIR,'/ANNDEA/Data/TRAIN_SET/','RawSeedsRes','MUTr1a','.csv',TrainSampleID,job_sets,'MUTr1a_GenerateRawSelectedSeeds_Sub.py'])
+prog_entry.append([AFS_DIR,EOS_DIR,PY_DIR,'/ANNDEA/Data/TRAIN_SET/','RawSeedsRes','MUTr1a','.csv',TrainSampleID,JobSets,'MUTr1a_GenerateRawSelectedSeeds_Sub.py'])
 prog_entry.append([ " --MaxSegments ", " --MaxSLG "," --MaxSTG "," --VetoMotherTrack "])
 prog_entry.append([MaxSegments, MaxSLG, MaxSTG,'"'+str(VetoMotherTrack)+'"'])
 prog_entry.append(TotJobs)
