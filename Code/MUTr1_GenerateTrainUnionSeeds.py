2e1d340168a6b0d9eb7e9c88941d57859ea0a3a5
Filips Fedotovs <74325502+FilipsFedotovs@users.noreply.github.com>
2023-03-28 12:56:05 +0100

    Update MUTr1_GenerateTrainUnionSeeds.py

--- Code/MUTr1_GenerateTrainUnionSeeds.py
+++ Code/MUTr1_GenerateTrainUnionSeeds.py
@@ -390,6 +390,7 @@ elif type(job_sets[0]) is int:
 elif type(job_sets[0][0]) is int:
                         for lp in job_sets:
                             TotJobs+=np.sum(lp)
+
 prog_entry.append(' Sending hit cluster to the HTCondor, so tack segment combination pairs can be formed...')
 prog_entry.append([AFS_DIR,EOS_DIR,PY_DIR,'/ANNDEA/Data/TRAIN_SET/','RawSeedsRes','MUTr1a','.csv',TrainSampleID,JobSets,'MUTr1a_GenerateRawSelectedSeeds_Sub.py'])
 prog_entry.append([ " --MaxSegments ", " --MaxSLG "," --MaxSTG "," --VetoMotherTrack "])
@@ -404,6 +405,7 @@ if Mode=='RESET':
 print(UF.TimeStamp(),UF.ManageTempFolders(prog_entry,'Create'))
 ###### Stage 1
 Program.append('Custom')
+
 ###### Stage 2
 prog_entry=[]
 job_sets=[]
