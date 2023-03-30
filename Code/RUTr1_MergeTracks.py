3e4c2e56aebc3bdb8b5666f0e7044f8d39663ae6
Filips <fedotovs@hotmail.com>
2023-03-30 21:20:36 +0100

    Updaing the Track merger

--- Code/RUTr1_MergeTracks.py
+++ Code/RUTr1_MergeTracks.py
@@ -449,8 +449,8 @@ print(UF.TimeStamp(),'Current status has a stage',Status,bcolors.ENDC)
 
 
 
-UpdateStatus(5)
-Status=5
+# UpdateStatus(5)
+# Status=5
 ################ Set the execution sequence for the script
 Program=[]
 
