39e86ec03f1d2672fd720b954c28a32124646a5f
Filips <fedotovs@hotmail.com>
2023-04-01 18:34:00 +0100

    Add merging option of the tracks with the overlap

--- Code/RUTr1_MergeTracks.py
+++ Code/RUTr1_MergeTracks.py
@@ -692,8 +692,8 @@ while Status<len(Program):
         print(UF.TimeStamp(), bcolors.OKGREEN+"Loading is successful, there are "+str(len(base_data))+" fit seeds..."+bcolors.ENDC)
         print(UF.TimeStamp(), "Stripping non-z information from seeds...")
         for tr in range(len(base_data)):
-            for t in range(len(tr.Hits)):
-                for h in range(len(t)):
+            for t in range(len(base_data[tr])):
+                for h in range(len(base_data[tr][t])):
                     base_data[tr][t][h]=base_data[tr][t][h]
             print(tr.Hits)
             x=input()
