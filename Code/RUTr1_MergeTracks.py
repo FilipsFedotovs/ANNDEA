abdc2da31826c03d87038fe297af82d96fb5424d
Filips <fedotovs@hotmail.com>
2023-04-01 21:00:25 +0100

    Add merging option of the tracks with the overlap

--- Code/RUTr1_MergeTracks.py
+++ Code/RUTr1_MergeTracks.py
@@ -692,14 +692,16 @@ while Status<len(Program):
         print(UF.TimeStamp(), "Loading fit track seeds from the file",bcolors.OKBLUE+input_file_location+bcolors.ENDC)
         base_data=UF.PickleOperations(input_file_location,'r','N/A')[0]
         print(UF.TimeStamp(), bcolors.OKGREEN+"Loading is successful, there are "+str(len(base_data))+" fit seeds..."+bcolors.ENDC)
-        maxx=0
+        minx=666666666666666666666
         with alive_bar(len(base_data),force_tty=True, title="Stripping non-z information from seeds...") as bar:
             for tr in range(len(base_data)):
                 bar()
                 for t in range(len(base_data[tr].Hits)):
                     for h in range(len(base_data[tr].Hits[t])):
-                        base_data[tr].Hits[t][h]=((base_data[tr].Hits[t][h][2])-332755)/1315
-
+                        base_data[tr].Hits[t][h]=((base_data[tr].Hits[t][h][2]))
+                        minx=min(base_data[tr].Hits[t][h],minx)
+        print(minx)
+        exit()
         # with alive_bar(len(base_data),force_tty=True, title="Remove overlapping hits from the seeds...") as bar:
         #     for tr in range(len(base_data)):
         #                 bar()
