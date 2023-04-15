e0bf603a6ae5fd47248423ea1b27040686988025
Filips <fedotovs@hotmail.com>
2023-04-15 17:56:46 +0100

    Add alignment

--- Code/RH_AlignBrick.py
+++ Code/RH_AlignBrick.py
@@ -86,77 +86,79 @@ new_combined_data = new_combined_data[new_combined_data.Track_Hit_No >= MinHits]
 new_combined_data=new_combined_data.drop(['Hit_ID','tx','ty'],axis=1)
 new_combined_data=new_combined_data.sort_values(['FEDRA_Track_ID','z'],ascending=[1,1])
 new_combined_data['FEDRA_Track_ID']=new_combined_data['FEDRA_Track_ID'].astype(int)
-
-Bad_Track_Pool=[]
-
-
-temp_data=new_combined_data[['FEDRA_Track_ID','x','y','z','Track_Hit_No']]
-
-Tracks_Head=temp_data[['FEDRA_Track_ID']]
-Tracks_Head.drop_duplicates(inplace=True)
-Tracks_List=temp_data.values.tolist() #I find it is much easier to deal with tracks in list format when it comes to fitting
-Tracks_Head=Tracks_Head.values.tolist()
-Bad_Track_Pool=[]
-#Bellow we build the track representatation that we can use to fit slopes
-with alive_bar(len(Tracks_Head),force_tty=True, title='Building track representations...') as bar:
-            for bth in Tracks_Head:
-               bar()
-               bth.append([])
-               bt=0
-               trigger=False
-               while bt<(len(Tracks_List)):
-                   if bth[0]==Tracks_List[bt][0]:
-
-                       bth[1].append(Tracks_List[bt][1:4])
-                       del Tracks_List[bt]
-                       bt-=1
-                       trigger=True
-                   elif trigger:
-                        break
-                   else:
-                        continue
-                   bt+=1
-with alive_bar(len(Tracks_Head),force_tty=True, title='Fitting the tracks...') as bar:
-     for bth in Tracks_Head:
-       bar()
-       x,y,z=[],[],[]
-       for b in bth[1]:
-           x.append(b[0])
-           y.append(b[1])
-           z.append(b[2])
-       tx=np.polyfit(z,x,1)[0]
-       ax=np.polyfit(z,x,1)[1]
-       ty=np.polyfit(z,y,1)[0]
-       ay=np.polyfit(z,y,1)[1]
-       bth.append(ax) #Append x intercept
-       bth.append(tx) #Append x slope
-       bth.append(0) #Append a placeholder slope (for polynomial case)
-       bth.append(ay) #Append x intercept
-       bth.append(ty) #Append x slope
-       bth.append(0) #Append a placeholder slope (for polynomial case)
-       del(bth[1])
-#Once we get coefficients for all tracks we convert them back to Pandas dataframe and join back to the data
-Tracks_Head=pd.DataFrame(Tracks_Head, columns = ['FEDRA_Track_ID','ax','t1x','t2x','ay','t1y','t2y'])
-
-temp_data=pd.merge(temp_data,Tracks_Head,how='inner',on = ['FEDRA_Track_ID'])
-print(UF.TimeStamp(),'Calculating x and y coordinates of the fitted line for all plates in the track...')
-#Calculating x and y coordinates of the fitted line for all plates in the track
-temp_data['new_x']=temp_data['ax']+(temp_data['z']*temp_data['t1x'])+((temp_data['z']**2)*temp_data['t2x'])
-temp_data['new_y']=temp_data['ay']+(temp_data['z']*temp_data['t1y'])+((temp_data['z']**2)*temp_data['t2y'])
-#Calculating how far hits deviate from the fit polynomial
-print(UF.TimeStamp(),'Calculating how far hits deviate from the fit polynomial...')
-temp_data['d_x']=temp_data['x']-temp_data['new_x']
-temp_data['d_y']=temp_data['y']-temp_data['new_y']
-temp_data['d_r']=temp_data['d_x']**2+temp_data['d_y']**2
-temp_data['d_r'] = temp_data['d_r'].astype(float)
-temp_data['d_r']=np.sqrt(temp_data['d_r']) #Absolute distance
-temp_data=temp_data[['FEDRA_Track_ID','Track_Hit_No','d_r']]
-temp_data=temp_data.groupby(['FEDRA_Track_ID','Track_Hit_No']).agg({'d_r':'sum'}).reset_index()
-temp_data=temp_data.agg({'d_r':'sum','Track_Hit_No':'sum'})
-temp_data=temp_data.values.tolist()
-fit=temp_data[0]/temp_data[1]
-print(fit)
+new_combined_data['Plate_ID']=new_combined_data['z'].astype(int)
+print(new_combined_data)
 exit()
+
+
+def FitPlate(PlateZ,)
+    Bad_Track_Pool=[]
+    temp_data=new_combined_data[['FEDRA_Track_ID','x','y','z','Track_Hit_No']]
+    Tracks_Head=temp_data[['FEDRA_Track_ID']]
+    Tracks_Head.drop_duplicates(inplace=True)
+    Tracks_List=temp_data.values.tolist() #I find it is much easier to deal with tracks in list format when it comes to fitting
+    Tracks_Head=Tracks_Head.values.tolist()
+    Bad_Track_Pool=[]
+    #Bellow we build the track representatation that we can use to fit slopes
+    with alive_bar(len(Tracks_Head),force_tty=True, title='Building track representations...') as bar:
+                for bth in Tracks_Head:
+                   bar()
+                   bth.append([])
+                   bt=0
+                   trigger=False
+                   while bt<(len(Tracks_List)):
+                       if bth[0]==Tracks_List[bt][0]:
+
+                           bth[1].append(Tracks_List[bt][1:4])
+                           del Tracks_List[bt]
+                           bt-=1
+                           trigger=True
+                       elif trigger:
+                            break
+                       else:
+                            continue
+                       bt+=1
+    with alive_bar(len(Tracks_Head),force_tty=True, title='Fitting the tracks...') as bar:
+         for bth in Tracks_Head:
+           bar()
+           x,y,z=[],[],[]
+           for b in bth[1]:
+               x.append(b[0])
+               y.append(b[1])
+               z.append(b[2])
+           tx=np.polyfit(z,x,1)[0]
+           ax=np.polyfit(z,x,1)[1]
+           ty=np.polyfit(z,y,1)[0]
+           ay=np.polyfit(z,y,1)[1]
+           bth.append(ax) #Append x intercept
+           bth.append(tx) #Append x slope
+           bth.append(0) #Append a placeholder slope (for polynomial case)
+           bth.append(ay) #Append x intercept
+           bth.append(ty) #Append x slope
+           bth.append(0) #Append a placeholder slope (for polynomial case)
+           del(bth[1])
+    #Once we get coefficients for all tracks we convert them back to Pandas dataframe and join back to the data
+    Tracks_Head=pd.DataFrame(Tracks_Head, columns = ['FEDRA_Track_ID','ax','t1x','t2x','ay','t1y','t2y'])
+
+    temp_data=pd.merge(temp_data,Tracks_Head,how='inner',on = ['FEDRA_Track_ID'])
+    print(UF.TimeStamp(),'Calculating x and y coordinates of the fitted line for all plates in the track...')
+    #Calculating x and y coordinates of the fitted line for all plates in the track
+    temp_data['new_x']=temp_data['ax']+(temp_data['z']*temp_data['t1x'])+((temp_data['z']**2)*temp_data['t2x'])
+    temp_data['new_y']=temp_data['ay']+(temp_data['z']*temp_data['t1y'])+((temp_data['z']**2)*temp_data['t2y'])
+    #Calculating how far hits deviate from the fit polynomial
+    print(UF.TimeStamp(),'Calculating how far hits deviate from the fit polynomial...')
+    temp_data['d_x']=temp_data['x']-temp_data['new_x']
+    temp_data['d_y']=temp_data['y']-temp_data['new_y']
+    temp_data['d_r']=temp_data['d_x']**2+temp_data['d_y']**2
+    temp_data['d_r'] = temp_data['d_r'].astype(float)
+    temp_data['d_r']=np.sqrt(temp_data['d_r']) #Absolute distance
+    temp_data=temp_data[['FEDRA_Track_ID','Track_Hit_No','d_r']]
+    temp_data=temp_data.groupby(['FEDRA_Track_ID','Track_Hit_No']).agg({'d_r':'sum'}).reset_index()
+    temp_data=temp_data.agg({'d_r':'sum','Track_Hit_No':'sum'})
+    temp_data=temp_data.values.tolist()
+    fit=temp_data[0]/temp_data[1]
+    print(fit)
+    exit()
 # print(new_combined_data)
 # exit()
 #     new_combined_data = new_combined_data.drop(["Rec_Seg_No"],axis=1)
