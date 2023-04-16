fe42b60ec42fdfce61e1b06aee555aa9d7b2a757
Filips <fedotovs@hotmail.com>
2023-04-16 15:14:47 +0100

    Add alignment

--- Code/RH_AlignBrick.py
+++ Code/RH_AlignBrick.py
@@ -177,8 +177,10 @@ for p in plates:
    def FitPlateFixed(x):
        return FitPlate(p[0],x,0,new_combined_data)
    res = minimize_scalar(FitPlateFixed, bounds=(-200, 200), method='bounded')
-   #new_combined_data=AlignPlate(p[0],)
-   print(res.x)
+
+   new_combined_data=AlignPlate(p[0],res.x,0,new_combined_data)
+   print(FitPlateFixed(0))
+
    exit()
 
 
