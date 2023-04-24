4cfc80220ae3690c122dcf0b34f3c23c90ce28f4
Filips <fedotovs@hotmail.com>
2023-04-24 21:44:52 +0100

    Add ability to Calibrate

--- Code/RUTr1_MergeTracks.py
+++ Code/RUTr1_MergeTracks.py
@@ -865,7 +865,7 @@ while Status<len(Program):
             print(combined_data)
             TP = combined_data['True'].sum()
             P = combined_data['True'].count()
-            Min_Acceptance=combined_data['Fit'].min()
+            Min_Acceptance=round(combined_data['Fit'].min(),1)
             FP=P-TP
             Ini_Precision=TP/P
             print(TP,P,FP,Ini_Precision,Min_Acceptance)
