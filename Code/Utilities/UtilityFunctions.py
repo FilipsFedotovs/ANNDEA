456a361c7527db46afbb2a1c2b79414ed56dea20
Filips <fedotovs@hotmail.com>
2023-04-25 23:11:58 +0100

    Fix the final merging step

--- Code/Utilities/UtilityFunctions.py
+++ Code/Utilities/UtilityFunctions.py
@@ -1478,7 +1478,7 @@ class EMO:
                           if (mel in res_vector) and (mel in final_vector)==False:
                              final_vector.append(mel)
                       for fel in f:
-                          if (fel in delete_vec)==False:
+                          if (fel in delete_vec)==False and (fel in final_vector)==False:
                              final_vector.append(fel)
                       return(final_vector)
       def ReplaceWeakerTracks(matx,m,f,m_fit,f_fit):
