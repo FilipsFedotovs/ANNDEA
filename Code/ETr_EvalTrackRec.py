81f9c0fa448717d9ad9c87aaeb9b560aad0db4c4
Filips <fedotovs@hotmail.com>
2023-04-26 19:26:39 +0100

    Fix the Evaluator script

--- Code/ETr_EvalTrackRec.py
+++ Code/ETr_EvalTrackRec.py
@@ -100,6 +100,7 @@ for rn in range(len(RecNames)):
     raw_data[TrackID[rn][1]] = raw_data[TrackID[rn][1]].astype(str)
     raw_data[RecNames[rn]] = raw_data[TrackID[rn][0]] + '-' + raw_data[TrackID[rn][1]]
     raw_data.drop([TrackID[rn][0],TrackID[rn][1]],axis=1,inplace=True)
+    print(TrackID[rn][0],TrackID[rn][1])
     print(RecNames[rn])
     x=input()
 print(raw_data)
