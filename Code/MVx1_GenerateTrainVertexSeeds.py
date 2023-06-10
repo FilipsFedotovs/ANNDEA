b2ddb8f5b49888bc2de68b7ae2f82db6c5c06d7d
henry-r-wilson <henryrwilson24@gmail.com>
2023-06-10 16:12:34 +0100

    MVx1 developement

--- Code/MVx1_GenerateTrainVertexSeeds.py
+++ Code/MVx1_GenerateTrainVertexSeeds.py
@@ -193,7 +193,7 @@ if os.path.isfile(required_file_location)==False or Mode=='RESET':
             data_aggregated=data_aggregated.rename(columns={'z': "PosBad_Z"})
             new_combined_data=pd.merge(new_combined_data, data_aggregated, how="left", on=['Rec_Seg_ID'])
             new_combined_data=pd.merge(new_combined_data, TracksZdf, how="left", left_on=["PosBad_Z"], right_on=['Bad_z'])
-            new_combined_data=new_combined_data[data['Bad_z'].isnull()]
+            new_combined_data=new_combined_data[new_combined_data['Bad_z'].isnull()]
             new_combined_data=new_combined_data.drop(['Bad_z', 'PosBad_Z'],axis=1)
         final_rows=len(data.axes[0])
         print(UF.TimeStamp(),'After removing tracks that start at the specific plates we have',final_rows,' hits left')
