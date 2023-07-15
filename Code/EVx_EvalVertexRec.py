709fd8a42315cdfe2761649c005982b53977dba2
henry-r-wilson <henryrwilson24@gmail.com>
2023-07-15 17:59:12 +0100

    EVx Development

--- Code/EVx_EvalVertexRec.py
+++ Code/EVx_EvalVertexRec.py
@@ -120,9 +120,7 @@ for rn in range(len(RecNames)):
     raw_data[VertexID[rn][0]] = raw_data[VertexID[rn][0]].astype(str)
     raw_data[VertexID[rn][1]] = raw_data[VertexID[rn][1]].astype(str)
     raw_data[RecNames[rn]] = raw_data[VertexID[rn][0]] + '-' + raw_data[VertexID[rn][1]]
-    print(raw_data)
-    exit()
-    raw_data.drop([VertexID[rn][0],VertexID[rn][1]],axis=1,inplace=True)
+    #raw_data.drop([VertexID[rn][0],VertexID[rn][1]],axis=1,inplace=True)
     if len(RemoveTracksZ)>0:
             print(UF.TimeStamp(),'Removing tracks based on start point')
             TracksZdf = pd.DataFrame(RemoveTracksZ, columns = ['Bad_z'], dtype=float)
@@ -132,6 +130,8 @@ for rn in range(len(RecNames)):
             raw_data=pd.merge(raw_data, TracksZdf, how="left", left_on=["PosBad_Z"], right_on=['Bad_z'])
             raw_data=raw_data[raw_data['Bad_z'].isnull()]
             raw_data=raw_data.drop(['Bad_z', 'PosBad_Z'],axis=1)
+print(raw_data)
+exit()
 if SkipRcmb:
     print(bcolors.HEADER+"#############################################################################################"+bcolors.ENDC)
     print(UF.TimeStamp(),bcolors.BOLD+'Stage 2:'+bcolors.ENDC+' Calculating recombination metrics...')
