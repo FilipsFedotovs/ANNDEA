fcaa426a294b06ae726236de748c500b79f49e17
Filips Fedotovs <74325502+FilipsFedotovs@users.noreply.github.com>
2023-08-17 20:50:41 +0100

    Add files via upload

---
+++ Code/Utilities/M_MergeSets.py
@@ -0,0 +1,71 @@
+#This simple script is design for studying and selecting an optimal size and resolution of the images
+
+########################################    Import libraries    #############################################
+import csv
+import argparse
+import ast
+import random
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+#Loading Directory locations
+csv_reader=open('../config',"r")
+config = list(csv.reader(csv_reader))
+for c in config:
+    if c[0]=='AFS_DIR':
+        AFS_DIR=c[1]
+    if c[0]=='EOS_DIR':
+        EOS_DIR=c[1]
+csv_reader.close()
+import sys
+sys.path.insert(1, AFS_DIR+'/Code/Utilities/')
+import UtilityFunctions as UF
+
+parser = argparse.ArgumentParser(description='This script helps to visualise the seeds by projecting their hit coordinates to the 2-d screen.')
+parser.add_argument('--TrainSampleID',help="What training sample to visualise?", default='SHIP_UR_v1')
+parser.add_argument('--NewTrainSampleID',help="What training sample to visualise?", default='SHIP_UR_v1')
+parser.add_argument('--f',help="Where are the sets located?", default='/eos/user/')
+parser.add_argument('--Sets',help="Name of the training sets?", default='[]')
+parser.add_argument('--Type',help="Please enter the sample type: VAL or TRAIN", default='1')
+########################################     Main body functions    #########################################
+args = parser.parse_args()
+TrainSampleID=args.TrainSampleID
+NewSampleID=args.NewTrainSampleID
+Sets=ast.literal_eval(args.Sets)
+input_location=args.f
+Type=args.Type
+
+
+
+print(bcolors.HEADER+"########################################################################################################"+bcolors.ENDC)
+print(bcolors.HEADER+"#########################   Initialising ANNDEA Training Set Merger            #########################"+bcolors.ENDC)
+print(bcolors.HEADER+"#########################              Written by Filips Fedotovs              #########################"+bcolors.ENDC)
+print(bcolors.HEADER+"#########################                 PhD Student at UCL                   #########################"+bcolors.ENDC)
+print(bcolors.HEADER+"########################################################################################################"+bcolors.ENDC)
+print(UF.TimeStamp(), bcolors.OKGREEN+"Modules Have been imported successfully..."+bcolors.ENDC)
+CombinedObject=[]
+for s in Sets:
+    input_file_location=input_location+'/'+s+'.pkl'
+    ObjectSet=UF.PickleOperations(input_file_location,'r', 'N/A')[0]
+    CombinedObject+=ObjectSet
+
+MetaFile=UF.PickleOperations(input_location+'/'+TrainSampleID+'_info.pkl','r', 'N/A')[0]
+random.shuffle(CombinedObject)
+print(MetaFile)
+
+
+
+
+
+
+
+
+
+
