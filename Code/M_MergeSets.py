68a02ce7468b7da9e6d6f627dfa945359d2d1b88
Filips Fedotovs <74325502+FilipsFedotovs@users.noreply.github.com>
2023-08-17 20:49:22 +0100

    Add files via upload

--- Code/M_MergeSets.py
+++ Code/M_MergeSets.py
@@ -31,7 +31,7 @@ import UtilityFunctions as UF
 parser = argparse.ArgumentParser(description='This script helps to visualise the seeds by projecting their hit coordinates to the 2-d screen.')
 parser.add_argument('--TrainSampleID',help="What training sample to visualise?", default='SHIP_UR_v1')
 parser.add_argument('--NewTrainSampleID',help="What training sample to visualise?", default='SHIP_UR_v1')
-parser.add_argument('--f',help="Where are the sets located??", default='/eos/user/')
+parser.add_argument('--f',help="Where are the sets located?", default='/eos/user/')
 parser.add_argument('--Sets',help="Name of the training sets?", default='[]')
 parser.add_argument('--Type',help="Please enter the sample type: VAL or TRAIN", default='1')
 ########################################     Main body functions    #########################################
@@ -56,121 +56,15 @@ for s in Sets:
     ObjectSet=UF.PickleOperations(input_file_location,'r', 'N/A')[0]
     CombinedObject+=ObjectSet
 
-
-print(CombinedObject)
+MetaFile=UF.PickleOperations(input_location+'/'+TrainSampleID+'.pkl','r', 'N/A')[0]
 random.shuffle(CombinedObject)
-print(CombinedObject)
-exit()
-# if args.Type=='VAL':
-#  input_file_location=EOS_DIR+'/ANNDEA/Data/TRAIN_SET/'+TrainSampleID+'_TRAIN_TRACK_OUTPUT_'+args.Type+'.pkl'
-#
-# ImageObjectSet=UF.PickleOperations(input_file_location,'r', 'N/A')[0]
-#
-# if args.Label=='Truth':
-#      ImageObjectSet=[im for im in ImageObjectSet if im.Label == 1]
-# if args.Label=='Fake':
-#      ImageObjectSet=[im for im in ImageObjectSet if im.Label == 0]
-# ImageObjectSet=ImageObjectSet[StartImage-1:min(TrackNo,len(ImageObjectSet))]
-# TrainSampleInputMeta=EOS_DIR+'/ANNDEA/Data/TRAIN_SET/'+TrainSampleID+'_info.pkl'
-# print(UF.TimeStamp(),'Loading the data file ',bcolors.OKBLUE+TrainSampleInputMeta+bcolors.ENDC)
-# MetaInput=UF.PickleOperations(TrainSampleInputMeta,'r', 'N/A')
-# print(MetaInput[1])
-# Meta=MetaInput[0]
-# DummyModelObj=UF.ModelMeta("Dummy'")
-# DummyModelObj.IniModelMeta([[],[],[],[],[],[],[],[],[],[],[],[MaxX,MaxY,MaxZ,resolution]], 'Tensorflow', Meta, EImg, 'CNN')
-#
-# #ImageObjectSet[0].PrepareSeedPrint(DummyModelObj)
-# #ImageObjectSet[0].Plot('XZ')
-#
-# if args.PlotType=='XZ':
-#   InitialData=[]
-#   Index=-1
-#   for x in range(-boundsX,boundsX):
-#           for z in range(0,boundsZ):
-#             InitialData.append(0.0)
-#   Matrix = np.array(InitialData)
-#   Matrix=np.reshape(Matrix,(H,L))
-# if args.PlotType=='YZ':
-#  InitialData=[]
-#  Index=-1
-#  for y in range(-boundsY,boundsY):
-#           for z in range(0,boundsZ):
-#             InitialData.append(0.0)
-#
-#  Matrix = np.array(InitialData)
-#  Matrix=np.reshape(Matrix,(W,L))
-# if args.PlotType=='XY':
-#   InitialData=[]
-#   Index=-1
-#   for x in range(-boundsX,boundsX):
-#           for y in range(-boundsY,boundsY):
-#             InitialData.append(0.0)
-#   Matrix = np.array(InitialData)
-#   Matrix=np.reshape(Matrix,(H,W))
-#
-#
-# Title=args.Label+' Track Image'
-#
-#
-# counter=0
-# for sd in ImageObjectSet:
-#  progress=int( round( (float(counter)/float(len(ImageObjectSet))*100),1)  )
-#  print('Rendering images, progress is ',progress, end="\r", flush=True)
-#  counter+=1
-#  sd.PrepareSeedPrint(DummyModelObj)
-#  if args.PlotType=='XZ':
-#   for Hits in sd.TrackPrint:
-#       if abs(Hits[0])<boundsX and abs(Hits[2])<boundsZ:
-#                    Matrix[Hits[0]+boundsX][Hits[2]]+=1
-#  if args.PlotType=='YZ':
-#         for Hits in sd.TrackPrint:
-#                  if abs(Hits[1])<boundsY and abs(Hits[2])<boundsZ:
-#                    Matrix[Hits[1]+boundsY][Hits[2]]+=1
-#  if args.PlotType=='XY':
-#      for Hits in sd.TrackPrint:
-#        if abs(Hits[0])<boundsX and abs(Hits[1])<boundsY:
-#          Matrix[Hits[0]+boundsX][Hits[1]+boundsY]+=1
-# image_no=len(ImageObjectSet)
-# del ImageObjectSet
-# import matplotlib as plt
-# from matplotlib.colors import LogNorm
-# import numpy as np
-# from matplotlib import pyplot as plt
-# if args.PlotType=='XZ':
-#  plt.title(Title)
-#  plt.xlabel('Z [microns /'+str(int(resolution))+']')
-#  plt.ylabel('X [microns /'+str(int(resolution))+']')
-#  if image_no==1:
-#     image=plt.imshow(Matrix,cmap='gray_r',extent=[0,boundsZ,boundsX,-boundsX])#,norm=LogNorm())
-#  else:
-#     image=plt.imshow(Matrix,cmap='gray_r',extent=[0,boundsZ,boundsX,-boundsX],norm=LogNorm())
-#  plt.gca().invert_yaxis()
-#  plt.show()
-# if args.PlotType=='YZ':
-#  import numpy as np
-#  from matplotlib import pyplot as plt
-#  plt.title(Title)
-#  plt.xlabel('Z [microns /'+str(int(resolution))+']')
-#  plt.ylabel('Y [microns /'+str(int(resolution))+']')
-#  if image_no==1:
-#     image=plt.imshow(Matrix,cmap='gray_r',extent=[0,boundsZ,boundsY,-boundsY])#,norm=LogNorm())
-#  else:
-#     image=plt.imshow(Matrix,cmap='gray_r',extent=[0,boundsZ,boundsY,-boundsY],norm=LogNorm())
-#  plt.gca().invert_yaxis()
-#  plt.show()
-# if args.PlotType=='XY':
-#  import numpy as np
-#  from matplotlib import pyplot as plt
-#  plt.title(Title)
-#  plt.xlabel('X [microns /'+str(int(resolution))+']')
-#  plt.ylabel('Y [microns /'+str(int(resolution))+']')
-#  if image_no==1:
-#     image=plt.imshow(Matrix,cmap='gray_r',extent=[boundsX,-boundsX,-boundsY,boundsY])#,norm=LogNorm())
-#  else:
-#     image=plt.imshow(Matrix,cmap='gray_r',extent=[boundsX,-boundsX,-boundsY,boundsY],norm=LogNorm())
-#  plt.gca().invert_xaxis()
-#  plt.show()
-# exit()
+print(MetaFile)
+
+
+
+
+
+
 
 
 
