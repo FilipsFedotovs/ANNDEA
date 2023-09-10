3e71602bb68cf415ce4ddf075574e32f0cc2c266
Filips Fedotovs <74325502+FilipsFedotovs@users.noreply.github.com>
2023-09-10 14:05:50 +0100

    Update RTr1a_ReconstructTracks_Sub.py

--- Code/Utilities/RTr1a_ReconstructTracks_Sub.py
+++ Code/Utilities/RTr1a_ReconstructTracks_Sub.py
@@ -168,7 +168,7 @@ for k in range(0,Z_ID_Max):
             LC_Value=ClusterData['z'].values[-1]
             print(EOS_DIR+p+'/Temp_'+pfx+'_'+RecBatchID+'_'+str(X_ID_n)+'/'+pfx+'_'+RecBatchID+'_'+o+'_'+str(X_ID_n)+'_'+str(Y_ID_n) +'_' +str(k)+'_CP'+sfx)
             print(LC_Label,LC_Value)
-            print(len(ClusterData)-1==LC_Value))
+            print(len(ClusterData)-1==LC_Value)
             exit()
             continue
     Z_ID=int(k)/Z_overlap
