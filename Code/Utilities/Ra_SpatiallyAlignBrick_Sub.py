524e4b64df59b681eecce66fb82c61fc6393f7c0
Filips Fedotovs <74325502+FilipsFedotovs@users.noreply.github.com>
2023-09-17 17:55:15 +0200

    Add files via upload

--- Code/Utilities/Ra_SpatiallyAlignBrick_Sub.py
+++ Code/Utilities/Ra_SpatiallyAlignBrick_Sub.py
@@ -146,7 +146,7 @@ def AlignPlate(PlateZ,dx,dy,input_data):
 
 #Specifying the full path to input/output files
 input_file_location=EOS_DIR+'/ANNDEA/Data/REC_SET/R_'+BatchID+'_HITS_'+str(j)+'_'+str(k)+'.csv'
-output_file_location=EOS_DIR+p+'/Temp_'+pfx+'_'+BatchID+'_'+str(i)+'/'+pfx+'_'+BatchID+o+str(i)+'_'+str(j)+'_'+str(k)+sfx
+output_file_location=EOS_DIR+p+'/Temp_'+pfx+'_'+BatchID+'_'+str(i)+'/'+pfx+'_'+BatchID+'_'+o+'_'+str(i)+'_'+str(j)+'_'+str(k)+sfx
 print(UF.TimeStamp(), "Modules Have been imported successfully...")
 print(UF.TimeStamp(),'Loading pre-selected data from ',input_file_location)
 data=pd.read_csv(input_file_location,header=0,
