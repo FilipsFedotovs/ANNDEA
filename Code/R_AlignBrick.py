c17946d726aab641ac0441881cff5b0a9451162f
Filips Fedotovs <74325502+FilipsFedotovs@users.noreply.github.com>
2023-09-19 21:08:28 +0100

    Add files via upload

--- Code/R_AlignBrick.py
+++ Code/R_AlignBrick.py
@@ -710,6 +710,12 @@ while Status<len(Program):
         data['k']=(data['y']-Min_y)/Size
         data['j']=data['j'].apply(np.floor)
         data['k']=data['k'].apply(np.floor)
+        print(UF.TimeStamp(),'Aligning spatial coordinates...')
+        data=pd.merge(data,spatial_alignment_map,on=['Plate_ID','j','k'],how='left')
+        data['dx'] = data['dx'].fillna(0.0)
+        data['dy'] = data['dy'].fillna(0.0)
+        data[PM.x]=data[PM.x]+data['dx']
+        data[PM.y]=data[PM.y]+data['dy']
         print(data)
         # print(UF.TimeStamp(),'Removing unreconstructed hits...')
         print('here')
