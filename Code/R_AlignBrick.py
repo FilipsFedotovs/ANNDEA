c9f0bff6ec9707668fa6454fa732842a3a4c0abe
Filips <fedotovs@hotmail.com>
2023-11-08 12:57:46 +0000

    Add GUI

--- Code/R_AlignBrick.py
+++ Code/R_AlignBrick.py
@@ -284,6 +284,7 @@ while Status<len(Program):
             for j in range(len(JobSets[i])):
                 for k in range(JobSets[i][j]):
                   result_file_location=EOS_DIR+'/ANNDEA/Data/REC_SET/Temp_Ra'+'_'+RecBatchID+'_'+str(i)+'/Ra_'+RecBatchID+'_SpatialAlignmentResult_'+Program[Status][22:]+'_'+str(i)+'_'+str(j)+'_'+str(k)+'.csv'
+                  print(UI.LogOperations(result_file_location,'r','N/A'))
                   result.append(UI.LogOperations(result_file_location,'r','N/A')[0])
         result=pd.DataFrame(result,columns=['Type','Plate_ID','j','k','dx','FitX','ValFitX','dy','FitY','ValFitY'])
         log_result=result
