db7368ba5c03f933f78adc24aa317fcb31d0562e
Filips <fedotovs@hotmail.com>
2023-11-30 17:36:42 +0000

    Adapt RUTr1 to the big data set

--- Code/RUTr1_MergeTracks.py
+++ Code/RUTr1_MergeTracks.py
@@ -651,8 +651,8 @@ while Status<len(Program):
                 data=data[ColUse]
                 if BrickID=='':
                     data[BrickID]='D'
-                UI.Msg('location','Loading mapped data from',EOS_DIR+'/ANNDEA/Data/REC_SET/RUTr1f_'+RecBatchID+'_Union_Tracks.csv')
-                map_data=pd.read_csv(EOS_DIR+'/ANNDEA/Data/REC_SET/RUTr1f_'+RecBatchID+'_Union_Tracks.csv',header=0)
+                UI.Msg('location','Loading mapped data from',EOS_DIR+'/ANNDEA/Data/REC_SET/RUTr1e_'+RecBatchID+'_Union_Tracks.csv')
+                map_data=pd.read_csv(EOS_DIR+'/ANNDEA/Data/REC_SET/RUTr1e_'+RecBatchID+'_Union_Tracks.csv',header=0)
                 total_rows=len(data.axes[0])
                 print(UI.TimeStamp(),'The raw data has ',total_rows,' hits')
                 print(UI.TimeStamp(),'Removing unreconstructed hits...')
@@ -661,7 +661,7 @@ while Status<len(Program):
                 print(UI.TimeStamp(),'The cleaned data has ',final_rows,' hits')
                 data[TrackID] = data[TrackID].astype(str)
                 data[BrickID] = data[BrickID].astype(str)
-                if os.path.isfile(EOS_DIR+'/ANNDEA/Data/REC_SET/RUTr1f_'+RecBatchID+'_Mapped_Tracks_Temp.csv')==False:
+                if os.path.isfile(EOS_DIR+'/ANNDEA/Data/REC_SET/RUTr1e_'+RecBatchID+'_Mapped_Tracks_Temp.csv')==False:
                     data['Rec_Seg_ID'] = data[TrackID] + '-' + data[BrickID]
                     print(UI.TimeStamp(),'Resolving duplicated hits...')
                     selected_combined_data=pd.merge(data, map_data, how="left", left_on=["Rec_Seg_ID"], right_on=['Old_Track_ID'])
