85beb654004d60595bc416942bfc7aa2fbbfcd7a
Filips <fedotovs@hotmail.com>
2023-12-03 17:50:42 +0000

    ANNDEA v3: RUTr1

--- Code/RUTr1_MergeTracks.py
+++ Code/RUTr1_MergeTracks.py
@@ -916,7 +916,7 @@ while Status<len(Program):
                                               print(UI.TimeStamp(),'The output '+str(i)+'  compression ratio is ', Compression_Ratio, ' %, skipping this step')
                             if Log:
                                          eval_data_file=EOS_DIR+'/ANNDEA/Data/TEST_SET/'+RecBatchID+'/EUTr1b_'+RecBatchID+'_SEED_TRUTH_COMBINATIONS.csv'
-                                         rec_list=list(set(rec_list))
+                                         rec_list=set(rec_list)
                                          eval_data=pd.read_csv(eval_data_file,header=0,usecols=['Segment_1','Segment_2'])
                                          eval_data["Seed_ID"]= ['-'.join(sorted(tup)) for tup in zip(eval_data['Segment_1'], eval_data['Segment_2'])]
                                          eval_data.drop(['Segment_1'],axis=1,inplace=True)
