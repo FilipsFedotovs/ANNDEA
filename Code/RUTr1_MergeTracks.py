35f5d7f1590d65507d84b6413594095bfa299026
Filips <fedotovs@hotmail.com>
2023-12-05 21:05:42 +0000

    ANNDEA v3: RUTr1

--- Code/RUTr1_MergeTracks.py
+++ Code/RUTr1_MergeTracks.py
@@ -792,7 +792,6 @@ while Status<len(Program):
                 new_combined_data.to_csv(final_output_file_location,index=False)
                 print(new_combined_data)
                 UI.Msg('location',"The merged track data has been created successfully and written to",final_output_file_location)
-                exit()
                 UI.UpdateStatus(Status+1,Meta,RecOutputMeta)
     else:
         for md in range(len(ModelName)):
