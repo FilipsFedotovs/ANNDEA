3f718228a5c5a7205e77afd82fd71ddd36762478
Filips <fedotovs@hotmail.com>
2023-12-10 14:50:08 +0000

    ANNDEA v3: RUTr1+EL9 switch

--- Code/Utilities/MUTr1b_RefineSeeds_Sub.py
+++ Code/Utilities/MUTr1b_RefineSeeds_Sub.py
@@ -93,14 +93,11 @@ input_track_file_location=EOS_DIR+'/ANNDEA/Data/TRAIN_SET/'+BatchID+'/Temp_MUTr1
 output_file_location=EOS_DIR+'/'+p+'/Temp_'+pfx+'_'+BatchID+'_'+str(i)+'/'+pfx+'_'+BatchID+'_'+o+'_'+str(i)+'_'+str(j)+sfx
 print(UI.TimeStamp(),'Loading the data')
 tracks=pd.read_csv(input_track_file_location)
-print(tracks)
-x=input()
 tracks_1=tracks.drop(['Segment_2'],axis=1)
 tracks_1=tracks_1.rename(columns={"Segment_1": "Rec_Seg_ID"})
 tracks_2=tracks.drop(['Segment_1'],axis=1)
 tracks_2=tracks_2.rename(columns={"Segment_2": "Rec_Seg_ID"})
 track_list=pd.concat([tracks_1,tracks_2])
-print(track_list)
 track_list=track_list.sort_values(['Rec_Seg_ID'])
 track_list.drop_duplicates(subset="Rec_Seg_ID",keep='first',inplace=True)
 segments=pd.read_csv(input_segment_file_location)
@@ -114,6 +111,9 @@ segments["ty"] = pd.to_numeric(segments["ty"],downcast='float')
 
 # reorder the columns
 segments = segments[['x','y','z','tx','ty', 'Rec_Seg_ID', 'MC_Mother_Track_ID', 'Seed_Type']]
+print(track_list)
+print(segments)
+x=input()
 segments = segments.values.tolist() #Convirting the result to List data type
 tracks = tracks.values.tolist() #Convirting the result to List data type
 
