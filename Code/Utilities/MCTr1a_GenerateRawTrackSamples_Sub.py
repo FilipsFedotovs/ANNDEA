923aa0466a45bc296463d82575846a26be5283ca
Filips <fedotovs@hotmail.com>
2024-03-16 17:33:44 +0000

    ANNDEA v3: MCTr1

--- Code/Utilities/MCTr1a_GenerateRawTrackSamples_Sub.py
+++ Code/Utilities/MCTr1a_GenerateRawTrackSamples_Sub.py
@@ -64,7 +64,7 @@ for j in ClassNames:
         if (k in ExtraColumns)==False:
             ExtraColumns.append(k)
 #Specifying the full path to input/output files
-input_file_location=EOS_DIR+'/ANNDEA/Data/TRAIN_SET/'+BatchID+'MCTr1_'+BatchID+'_TRACKS.csv'
+input_file_location=EOS_DIR+'/ANNDEA/Data/TRAIN_SET/'+BatchID+'/MCTr1_'+BatchID+'_TRACKS.csv'
 output_file_location=EOS_DIR+p+'/Temp_'+pfx+'_'+BatchID+'_0/'+pfx+'_'+BatchID+'_'+o+'_'+str(i)+sfx
 
 print(UI.TimeStamp(), "Modules Have been imported successfully...")
