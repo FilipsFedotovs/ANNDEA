798ab070e75e15afa4df8e43b7e9be2e267436b9
Filips <fedotovs@hotmail.com>
2024-03-16 14:28:48 +0000

    ANNDEA v3: MCTr1

--- Code/MCTr1_GenerateTrainClassifyTracks.py
+++ Code/MCTr1_GenerateTrainClassifyTracks.py
@@ -154,7 +154,8 @@ if os.path.isfile(required_file_location)==False:
             data_aggregated_show=data_aggregated.groupby(['z']).count().reset_index()
             data_aggregated_show=data_aggregated_show.rename(columns={'Rec_Seg_ID': "No_Tracks"})
             data_aggregated_show['PID']=data_aggregated_show['z'].rank(ascending=True).astype(int)
-            print(data_aggregated_show)
+            RPChoice = input('Enter the list of plates separated by comma that you want to remove followed by "Enter" : ')
+            print(RPChoice)
             exit()
 
             data_aggregated=data_aggregated.rename(columns={'z': "PosBad_Z"})
