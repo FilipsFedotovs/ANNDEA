e765ee6fa7364cfde2ce9f7b4c8bd25a052fc273
Filips <fedotovs@hotmail.com>
2024-03-18 21:14:26 +0000

    ANNDEA v3: M_Model

--- Code/Utilities/U_ML.py
+++ Code/Utilities/U_ML.py
@@ -900,10 +900,15 @@ def GenerateModel(ModelMeta,TrainParams=None):
                          DR=float(FC[2]-1)/10.0
                          model.add(Dense(Nodes, activation=act_fun_list[FC[1]], kernel_initializer='he_uniform'))
                          model.add(Dropout(DR))
-            model.add(Dense(OutputLayer[1], activation=act_fun_list[OutputLayer[0]]))
-            opt = Adam(learning_rate=TrainParams[0])
+            if OutputLayer[1]==1:
+                model.add(Dense(OutputLayer[1]))
+                opt = Adam(learning_rate=TrainParams[0])
+                model.compile(loss='mean_squared_loss',optimizer=opt,metrics=['mse'])
+            else:
+                model.add(Dense(OutputLayer[1], activation=act_fun_list[OutputLayer[0]]))
+                opt = Adam(learning_rate=TrainParams[0])
+                model.compile(loss='categorical_crossentropy',optimizer=opt,metrics=['accuracy'])
      # Compile the model
-            model.compile(loss='categorical_crossentropy',optimizer=opt,metrics=['accuracy'])
             return model
 
 
