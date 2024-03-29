28d4f8eb1b79a8be2273213978ca498065658c6b
Filips Fedotovs <74325502+FilipsFedotovs@users.noreply.github.com>
2024-03-29 15:15:56 +0000

    Update MCTr2_TrainModel_Sub.py

--- Code/Utilities/MCTr2_TrainModel_Sub.py
+++ Code/Utilities/MCTr2_TrainModel_Sub.py
@@ -159,6 +159,7 @@ def main(self):
         import warnings
         warnings.simplefilter(action='ignore', category=FutureWarning)
         import tensorflow as tf
+        sess = tf.InteractiveSession()
         K = tf.keras.backend 
         # try:
         model=tf.keras.models.load_model(Model_Path)
