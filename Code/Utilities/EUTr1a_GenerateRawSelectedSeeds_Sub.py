b72c4112a65f7e84d73d23cb80d22bb647cbbb89
Filips <fedotovs@hotmail.com>
2024-04-30 20:55:41 +0100

    ANNDEA v3: Developing RVx1

--- Code/Utilities/EUTr1a_GenerateRawSelectedSeeds_Sub.py
+++ Code/Utilities/EUTr1a_GenerateRawSelectedSeeds_Sub.py
@@ -34,11 +34,11 @@ AFS_DIR=args.AFS
 PY_DIR=args.PY
 if PY_DIR!='': #Temp solution
     sys.path=['',PY_DIR]
-    sys.path.append('/usr/lib64/python36.zip')
-    sys.path.append('/usr/lib64/python3.6')
-    sys.path.append('/usr/lib64/python3.6/lib-dynload')
-    sys.path.append('/usr/lib64/python3.6/site-packages')
-    sys.path.append('/usr/lib/python3.6/site-packages')
+    sys.path.append('/usr/lib64/python39.zip')
+    sys.path.append('/usr/lib64/python3.9')
+    sys.path.append('/usr/lib64/python3.9/lib-dynload')
+    sys.path.append('/usr/lib64/python3.9/site-packages')
+    sys.path.append('/usr/lib/python3.9/site-packages')
 sys.path.append(AFS_DIR+'/Code/Utilities')
 import U_UI as UI #This is where we keep routine utility functions
 import pandas as pd #We use Panda for a routine data processing
