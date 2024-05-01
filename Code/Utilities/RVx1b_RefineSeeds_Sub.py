e89e7dd8a789cda9b8c4839286ffe3136350c707
Filips <fedotovs@hotmail.com>
2024-05-01 23:02:37 +0100

    ANNDEA v3: Developing RVx1

--- Code/Utilities/RVx1b_RefineSeeds_Sub.py
+++ Code/Utilities/RVx1b_RefineSeeds_Sub.py
@@ -166,7 +166,7 @@ if FirstTime=='True':
     print(UF.PickleOperations(output_file_location,'w', GoodTracks)[1])
 else:
     input_seed_file_location=EOS_DIR+'/ANNDEA/Data/REC_SET/'+BatchID+'/Temp_RVx1'+FirstTime+'_'+BatchID+'_0/RVx1'+str(ModelName)+'_'+BatchID+'_Input_Seeds_'+str(i)+'_'+str(j)+'.pkl'
-    output_file_location=EOS_DIR+'/'+p+'/Temp_'+pfx+'_'+BatchID+'_0/'+pfx+'_'+BatchID+'_'+o+'_'+str(i)+sfx
+    output_file_location=EOS_DIR+'/'+p+'/Temp_'+pfx+'_'+BatchID+'_0/'+pfx+'_'+BatchID+'_'+o+'_'+str(i)+'_'+str(j)+sfx
     print(UF.TimeStamp(),'Loading the data')
     seeds=UF.PickleOperations(input_seed_file_location,'r','N/A')[0]
     print(UF.TimeStamp(),bcolors.OKGREEN+'Data has been successfully loaded and prepared..'+bcolors.ENDC)
