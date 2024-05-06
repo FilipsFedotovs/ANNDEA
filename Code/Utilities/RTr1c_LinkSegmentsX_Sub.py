7273cdbacf0a56ae60d46b8c34c3a9860dd9db17
Filips <fedotovs@hotmail.com>
2024-05-06 19:14:05 +0100

    ANNDEA v3: Developing MTr2 - optimise CNN training

--- Code/Utilities/RTr1c_LinkSegmentsX_Sub.py
+++ Code/Utilities/RTr1c_LinkSegmentsX_Sub.py
@@ -33,17 +33,17 @@ EOS_DIR=args.EOS
 PY_DIR=args.PY
 if PY_DIR!='': #Temp solution
     sys.path=['',PY_DIR]
-    sys.path.append('/usr/lib64/python36.zip')
-    sys.path.append('/usr/lib64/python3.6')
-    sys.path.append('/usr/lib64/python3.6/lib-dynload')
-    sys.path.append('/usr/lib64/python3.6/site-packages')
-    sys.path.append('/usr/lib/python3.6/site-packages')
+    sys.path.append('/usr/lib64/python39.zip')
+    sys.path.append('/usr/lib64/python3.9')
+    sys.path.append('/usr/lib64/python3.9/lib-dynload')
+    sys.path.append('/usr/lib64/python3.9/site-packages')
+    sys.path.append('/usr/lib/python3.9/site-packages')
 sys.path.append(AFS_DIR+'/Code/Utilities')
 
 #import the rest of the libraries
 import pandas as pd
 pd.options.mode.chained_assignment = None #Silence annoying warnings
-import UtilityFunctions as UF
+import U_UI as UI
 from alive_progress import alive_bar
 #Load data configuration
 EOSsubDIR=EOS_DIR+'/'+'ANNDEA'
@@ -61,12 +61,12 @@ def zero_divide(a, b):
     if (b==0): return 0
     return a/b
 #Load the first file (on the y-axis) with reconstructed clusters that already have been merged along z-axis
-FirstFileName=EOS_DIR+'/ANNDEA/Data/REC_SET/Temp_RTr1b_'+RecBatchID+'_'+str(0)+'/RTr1b_'+RecBatchID+'_hit_cluster_rec_y_set_' +str(0)+'.csv'
+FirstFileName=EOS_DIR+'/ANNDEA/Data/REC_SET/'+RecBatchID+'/Temp_RTr1b_'+RecBatchID+'_'+str(0)+'/RTr1b_'+RecBatchID+'_hit_cluster_rec_y_set_' +str(0)+'.csv'
 ZContractedTable=pd.read_csv(FirstFileName)  #First cluster is like a Pacman: it absorbes proceeding clusters and gets bigger
 with alive_bar(X_ID_Max-1,force_tty=True, title='Merging cluster sets along x-axis..') as bar:
     for i in range(1,X_ID_Max):
         bar()
-        SecondFileName=EOS_DIR+'/ANNDEA/Data/REC_SET/Temp_RTr1b_'+RecBatchID+'_'+str(0)+'/RTr1b_'+RecBatchID+'_hit_cluster_rec_y_set_'+str(i)+'.csv' #keep loading subsequent files along y-xis with reconstructed clusters that already have been merged along z and y-axis
+        SecondFileName=EOS_DIR+'/ANNDEA/Data/REC_SET/'+RecBatchID+'/Temp_RTr1b_'+RecBatchID+'_'+str(0)+'/RTr1b_'+RecBatchID+'_hit_cluster_rec_y_set_'+str(i)+'.csv' #keep loading subsequent files along y-xis with reconstructed clusters that already have been merged along z and y-axis
         SecondFile=pd.read_csv(SecondFileName)
         SecondFileTable=SecondFile.rename(columns={"Master_Segment_ID":"Segment_ID","Master_z":"z" }) #Initally the following clusters are downgraded from the master status
         FileClean=pd.merge(ZContractedTable.drop_duplicates(subset=["Master_Segment_ID","HitID",'Master_z'],keep='first'),SecondFileTable,how='inner', on=['HitID']) #Join segments based on the common hits
@@ -124,5 +124,5 @@ ZContractedTable.drop(['Master_z',"Master_Segment_ID"],axis=1,inplace=True)
 ZContractedTable[RecBatchID+'_Brick_ID']=RecBatchID #Creating the track prefix relevant to this particular reconstruction (to keep track IDs unique)
 output_file_location=EOS_DIR+p+'/Temp_'+pfx+'_'+RecBatchID+'_'+str(0)+'/'+pfx+'_'+RecBatchID+'_'+o+'_'+str(0)+sfx
 ZContractedTable.to_csv(output_file_location,index=False)
-print(UF.TimeStamp(),'Output is written to ',output_file_location) #Write the output
+print(UI.TimeStamp(),'Output is written to ',output_file_location) #Write the output
 exit()
