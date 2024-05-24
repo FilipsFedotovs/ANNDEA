ab6959d2a3107b83dff317193f4d384a7498d966
Filips <fedotovs@hotmail.com>
2024-05-24 22:00:37 +0100

    ANNDEA v3: Developing RVx1 - introduce Callibration

--- Code/RVx1_ReconstructVertexes.py
+++ Code/RVx1_ReconstructVertexes.py
@@ -690,7 +690,7 @@ while Status<len(Program):
         base_data['Seed_Index'] = base_data.index
         base_data.drop(base_data.index[base_data['Seed_Link_Fit'] < LinkAcceptance],inplace=True)  # Dropping the seeds that don't pass the link fit threshold
         if CalibrateAcceptance:
-            print(UF.TimeStamp(),'Calibrating the acceptance...')
+            print(UI.TimeStamp(),'Calibrating the acceptance...')
             eval_data_file=EOS_DIR+'/ANNDEA/Data/TEST_SET/EUTr1b_'+RecBatchID+'_SEED_TRUTH_COMBINATIONS.csv'
             eval_data=pd.read_csv(eval_data_file,header=0,usecols=['Segment_1','Segment_2'])
             eval_data["Seed_ID"]= ['-'.join(sorted(tup)) for tup in zip(eval_data['Segment_1'], eval_data['Segment_2'])]
