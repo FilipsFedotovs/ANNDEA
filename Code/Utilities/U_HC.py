201fe393118ef320417a9fcea55eecaaf9e5cc73
Filips <fedotovs@hotmail.com>
2024-08-22 22:19:53 +0100

    ANNDEA v3: Developing RTr1 - Optimising tracking perfromance

--- Code/Utilities/U_HC.py
+++ Code/Utilities/U_HC.py
@@ -126,7 +126,7 @@ class HitCluster:
            #######Next####
            import datetime
            T1=datetime.datetime.now()
-           # import pandas as pd
+           import pandas as pd
            _Tot_Hits_b=pd.DataFrame(_Tot_Hits, columns = ['l_HitID','l_x','l_y','l_z','l_tx','l_ty','r_HitID','r_x','r_y','r_z','r_tx','r_ty'])
            # self.HitPairs=_Tot_Hits[['l_HitID','l_z','r_HitID','r_z']]
            print(_Tot_Hits[0])
