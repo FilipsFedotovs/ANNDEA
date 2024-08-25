fcacde6915072aa7f3036e804386ac57b65be245
Filips <fedotovs@hotmail.com>
2024-08-25 14:30:14 +0100

    ANNDEA v3: Developing RTr1 - Optimising tracking perfromance - Remove Pandas

--- Code/Utilities/RTr1a_ReconstructTracks_Sub.py
+++ Code/Utilities/RTr1a_ReconstructTracks_Sub.py
@@ -196,7 +196,7 @@ for k in range(0,Z_ID_Max):
                             for edge in range(len(HC.edges)):
                                 combined_weight_list.append(HC.edges[edge]+w[edge]) #Join the Hit Pair classification back to the hit pairs
                             combined_weight_list=pd.DataFrame(combined_weight_list, columns = ['l_HitID','r_HitID','link_strength'])
-                            _HitPairs=pd.dataframe(HC.HitPairs, columns=['l_HitID','l_z','r_HitID','r_z'])
+                            _HitPairs=pd.DataFrame(HC.HitPairs, columns=['l_HitID','l_z','r_HitID','r_z'])
                             print(HC.HitPairs)
                             _Tot_Hits=pd.merge(_HitPairs, combined_weight_list, how="inner", on=['l_HitID','r_HitID'])
                             print(_Tot_Hits)
