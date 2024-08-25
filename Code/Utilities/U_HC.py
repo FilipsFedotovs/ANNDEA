3a8eac5d230b9137931b579e0f10479241b84898
Filips <fedotovs@hotmail.com>
2024-08-25 13:42:05 +0100

    ANNDEA v3: Developing RTr1 - Optimising tracking perfromance - Remove Pandas

--- Code/Utilities/U_HC.py
+++ Code/Utilities/U_HC.py
@@ -126,34 +126,37 @@ class HitCluster:
            #######Next####
            import datetime
            T1=datetime.datetime.now()
-           #import pandas as pd
-           #_Tot_Hits_b=pd.DataFrame(_Tot_Hits, columns = ['l_HitID','l_x','l_y','l_z','l_tx','l_ty','r_HitID','r_x','r_y','r_z','r_tx','r_ty'])
+           import pandas as pd
+           _Tot_Hits_b=pd.DataFrame(_Tot_Hits, columns = ['l_HitID','l_x','l_y','l_z','l_tx','l_ty','r_HitID','r_x','r_y','r_z','r_tx','r_ty'])
            # self.HitPairs=_Tot_Hits[['l_HitID','l_z','r_HitID','r_z']]
            print(_Tot_Hits[0])
            print(_Tot_Hits[44])
            for TH in _Tot_Hits:
                for i in range(1,4):TH[i]=TH[i]/self.Step[2]
                for i in range(7,10):TH[i]=TH[i]/self.Step[2]
-           _Tot_Hits =  [x[1] / self.Step[2] for x in _Tot_Hits]
+               TH.append((math.sqrt(((TH[8]-TH[2])**2) + ((TH[7]-TH[1])**2) + ((TH[9]-TH[3])**2))))
            print(_Tot_Hits[0])
-           print(_Tot_Hits[44])
-           # _Tot_Hits_b['l_x']=_Tot_Hits_b['l_x']/self.Step[2]
-           # _Tot_Hits_b['l_y']=_Tot_Hits_b['l_y']/self.Step[2]
-           # _Tot_Hits_b['l_z']=_Tot_Hits_b['l_z']/self.Step[2]
-           # _Tot_Hits_b['r_x']=_Tot_Hits_b['r_x']/self.Step[2]
-           # _Tot_Hits_b['r_y']=_Tot_Hits_b['r_y']/self.Step[2]
-           # _Tot_Hits_b['r_z']=_Tot_Hits_b['r_z']/self.Step[2]
-           # print(_Tot_Hits_b)
-           # _Tot_Hits['label']='N/A'
-           # _Tot_Hits['d_l'] = (np.sqrt(((_Tot_Hits['r_y']-_Tot_Hits['l_y'])**2) + ((_Tot_Hits['r_x']-_Tot_Hits['l_x'])**2) + ((_Tot_Hits['r_z']-_Tot_Hits['l_z'])**2)))
-           # _Tot_Hits['d_t'] = np.sqrt(((_Tot_Hits['r_y']-_Tot_Hits['l_y'])**2) + ((_Tot_Hits['r_x']-_Tot_Hits['l_x'])**2))
-           # _Tot_Hits['d_z'] = (_Tot_Hits['r_z']-_Tot_Hits['l_z']).abs()
-           # _Tot_Hits['d_tx'] = _Tot_Hits['l_tx']-_Tot_Hits['r_tx']
-           # _Tot_Hits['d_tx'] = _Tot_Hits['d_tx'].abs()
-           # _Tot_Hits['d_ty'] = _Tot_Hits['l_ty']-_Tot_Hits['r_ty']
-           # _Tot_Hits['d_ty'] = _Tot_Hits['d_ty'].abs()
-           # _Tot_Hits = _Tot_Hits.drop(['r_x','r_y','r_z','l_x','l_y','l_z'],axis=1)
-           # _Tot_Hits=_Tot_Hits[['l_HitID','r_HitID','label','d_l','d_t','d_z','d_tx','d_ty']]
+           print(_Tot_Hits[2])
+           print(_Tot_Hits[3])
+           _Tot_Hits_b['l_x']=_Tot_Hits_b['l_x']/self.Step[2]
+           _Tot_Hits_b['l_y']=_Tot_Hits_b['l_y']/self.Step[2]
+           _Tot_Hits_b['l_z']=_Tot_Hits_b['l_z']/self.Step[2]
+           _Tot_Hits_b['r_x']=_Tot_Hits_b['r_x']/self.Step[2]
+           _Tot_Hits_b['r_y']=_Tot_Hits_b['r_y']/self.Step[2]
+           _Tot_Hits_b['r_z']=_Tot_Hits_b['r_z']/self.Step[2]
+
+           _Tot_Hits_b['d_l'] = (np.sqrt(((_Tot_Hits_b['r_y']-_Tot_Hits_b['l_y'])**2) + ((_Tot_Hits_b['r_x']-_Tot_Hits_b['l_x'])**2) + ((_Tot_Hits_b['r_z']-_Tot_Hits_b['l_z'])**2)))
+           # _Tot_Hits_b['d_t'] = np.sqrt(((_Tot_Hits_b['r_y']-_Tot_Hits_b['l_y'])**2) + ((_Tot_Hits_b['r_x']-_Tot_Hits_b['l_x'])**2))
+           # _Tot_Hits_b['d_z'] = (_Tot_Hits_b['r_z']-_Tot_Hits_b['l_z']).abs()
+
+           print(_Tot_Hits_b)
+
+           # _Tot_Hits_b['d_tx'] = _Tot_Hits_b['l_tx']-_Tot_Hits_b['r_tx']
+           # _Tot_Hits_b['d_tx'] = _Tot_Hits_b['d_tx'].abs()
+           # _Tot_Hits_b['d_ty'] = _Tot_Hits_b['l_ty']-_Tot_Hits_b['r_ty']
+           # _Tot_Hits_b['d_ty'] = _Tot_Hits_b['d_ty'].abs()
+           # _Tot_Hits = _Tot_Hits_b.drop(['r_x','r_y','r_z','l_x','l_y','l_z'],axis=1)
+           # _Tot_Hits=_Tot_Hits_b[['l_HitID','r_HitID','label','d_l','d_t','d_z','d_tx','d_ty']]
            #
            # _Tot_Hits=_Tot_Hits.values.tolist()
 
