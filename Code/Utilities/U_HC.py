35bd86669b84b8ca259a36df467bb11930cdd9ba
Filips <fedotovs@hotmail.com>
2024-08-25 14:02:04 +0100

    ANNDEA v3: Developing RTr1 - Optimising tracking perfromance - Remove Pandas

--- Code/Utilities/U_HC.py
+++ Code/Utilities/U_HC.py
@@ -141,7 +141,7 @@ class HitCluster:
                TH.append(abs(TH[4]-TH[10]))
                TH.append(abs(TH[5]-TH[11]))
                del TH[1:6]
-               del TH[4:8]
+               #del TH[4:8]
            print(_Tot_Hits[0])
            _Tot_Hits_b['l_x']=_Tot_Hits_b['l_x']/self.Step[2]
            _Tot_Hits_b['l_y']=_Tot_Hits_b['l_y']/self.Step[2]
