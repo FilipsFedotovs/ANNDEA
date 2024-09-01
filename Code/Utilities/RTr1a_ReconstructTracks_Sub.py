d677a4bd1b8ef39f28af9825547ca5d45e0dd51b
Filips <fedotovs@hotmail.com>
2024-09-01 15:57:18 +0100

    ANNDEA v3: Developing RTr1 - Linux RH

--- Code/Utilities/RTr1a_ReconstructTracks_Sub.py
+++ Code/Utilities/RTr1a_ReconstructTracks_Sub.py
@@ -394,7 +394,6 @@ if Status=='Tracking' or Status=='Tracking continuation':
                     for c in range(column_no):
                         _Tot_Hits_Predator.drop_duplicates(subset=[str(c)], keep='first', inplace=True) #Iterating over hits, make sure that they belong to the best-fit track
                     _Tot_Hits_Predator=_Tot_Hits_Predator.values.tolist()
-                    print(_Tot_Hits_Predator)
                     for seg in range(len(_Tot_Hits_Predator)):
                         _Tot_Hits_Predator[seg]=[s for s in _Tot_Hits_Predator[seg] if ('H' in s)==False] #Remove holes from the track representation
                     _Rec_Hits_Pool+=_Tot_Hits_Predator
@@ -418,9 +417,14 @@ if Status=='Tracking' or Status=='Tracking continuation':
                   for h in _Rec_Hits_Pool[t]:
                          _track_list.append([_segment_id+'-'+str(t+1),h])
     _Rec_Hits_Pool=pd.DataFrame(_track_list, columns = ['Segment_ID','HitID'])
+    print(_Rec_Hits_Pool)
+    print(_z_map)
+    print(_Rec_Hits_Pool.dtypes)
+    print(_z_map.dtypes)
+    exit()
     _Rec_Hits_Pool=pd.merge(_z_map, _Rec_Hits_Pool, how="right", on=['HitID'])
     print(UI.TimeStamp(),_no_tracks, 'track segments have been reconstructed in this cluster set ...')
-    print(_Rec_Hits_Pool)
+
     exit()
     # exit()
     #
