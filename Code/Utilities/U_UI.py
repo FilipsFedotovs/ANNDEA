a3b98a7de51609ca2273cabfdfcd8777a9a7837a
Filips <fedotovs@hotmail.com>
2024-11-05 18:34:38 +0000

    ANNDEA: Redistribute edge generation

--- Code/Utilities/U_UI.py
+++ Code/Utilities/U_UI.py
@@ -214,6 +214,8 @@ def CreateCondorJobs(AFS,EOS,PY,path,o,pfx,sfx,ID,loop_params,OptionHeader,Optio
         TotJobs=0
         TotJobs=CalculateNJobs(loop_params)[1]
         nest_lvl=CalculateNJobs(loop_params)[0]
+        print(nest_lvl,TotJobs)
+        exit()
         OH=OptionHeader+[' --EOS '," --AFS ", " --PY ", " --BatchID "]
         OL=OptionLine+[EOS, AFS, PY, ID]
         TotJobs=int(TotJobs)
