70586a1c9ab0ca6fefbd607a5debbbf68312b91b
Filips <fedotovs@hotmail.com>
2024-11-06 16:33:47 +0000

    ANNDEA: Redistribute edge generation

--- Code/Utilities/RTr1a_GenerateEdges_Sub.py
+++ Code/Utilities/RTr1a_GenerateEdges_Sub.py
@@ -1,14 +1,8 @@
-#Current version 1.1 - add change sys path capability
+#Current version 1.2 - redistribute edge generation
 
 ########################################    Import essential libriries    #############################################
 import argparse
 import sys
-import copy
-from statistics import mean
-import os
-import ast
-
-
 #Setting the parser - this script is usually not run directly, but is used by a Master version Counterpart that passes the required arguments
 parser = argparse.ArgumentParser(description='select cut parameters')
 parser.add_argument('--i',help="Set number", default='1')
@@ -27,14 +21,12 @@ parser.add_argument('--p',help="Path to the output file", default='')
 parser.add_argument('--o',help="Path to the output file name", default='')
 parser.add_argument('--pfx',help="Path to the output file name", default='')
 parser.add_argument('--sfx',help="Path to the output file name", default='')
-
 #Working out where are the Py libraries
 args = parser.parse_args()
 #Loading Directory locations
 EOS_DIR=args.EOS
 AFS_DIR=args.AFS
 PY_DIR=args.PY
-
 if PY_DIR!='': #Temp solution
     sys.path=['',PY_DIR]
     sys.path.append('/usr/lib64/python39.zip')
@@ -43,41 +35,21 @@ if PY_DIR!='': #Temp solution
     sys.path.append('/usr/lib64/python3.9/site-packages')
     sys.path.append('/usr/lib/python3.9/site-packages')
 sys.path.append(AFS_DIR+'/Code/Utilities')
-import pandas as pd #We use Panda for a routine data processing
-import numpy as np
 ######################################## Set variables  #############################################################
 cut_dt,cut_dr,cut_dz=float(args.cut_dt),float(args.cut_dr),float(args.cut_dz)
 p,o,sfx,pfx=args.p,args.o,args.sfx,args.pfx
 RecBatchID=args.BatchID
 MaxEdgesPerJob=int(args.MaxEdgesPerJob)
 i,j,k,l=args.i,args.j,args.k,args.l
-
 import U_UI as UI #This is where we keep routine utility functions
-# import U_HC as HC_l
-# import U_ML as ML
-
 input_file_location=EOS_DIR+p+'/RTr1_'+RecBatchID+'_'+str(i)+'_'+str(j)+'_'+str(k)+'_clusters.pkl'
 HC=UI.PickleOperations(input_file_location,'r','')[0]
-print(HC.RawClusterGraph)
 print(UI.TimeStamp(),'Generating the edges...')
-#print(UI.TimeStamp(),"Hit density of the Cluster",round(X_ID,1),round(Y_ID,1),1, "is  {} hits per cm\u00b3".format(round(len(HC.RawClusterGraph)/(stepX/10000*stepY/10000*stepZ/10000)),2))
 GraphStatus = HC.GenerateEdges(cut_dt, cut_dr, cut_dz, [], int(l), MaxEdgesPerJob)
+print(UI.TimeStamp(),'Writing the output...')
+output_file_location=EOS_DIR+p+'/Temp_'+pfx+'_'+RecBatchID+'_'+str(i)+'_'+str(j)+'_'+str(k)+'/'+pfx+'_'+RecBatchID+'_'+o+'_'+str(i)+'_'+str(j)+'_'+str(k)+'_'+str(l)+sfx
+UI.PickleOperations(output_file_location,'w',HC)
+print(UI.TimeStamp(),'Output is written to ',output_file_location)
 exit()
-print(GraphStatus)
-print(HC.RawEdgeGaph)
-
-# if CheckPoint and GraphStatus:
-#     print(UI.TimeStamp(),'Saving checkpoint 2...')
-#     UI.PickleOperations(CheckPointFile_Edge,'w',HC)
-# if GraphStatus:
-#     Status = 'ML analysis'
-# else:
-#     Status = 'Skip tracking'
-
-# output_file_location=EOS_DIR+p+'/Temp_'+pfx+'_'+RecBatchID+'_'+str(args.i)+'_'+str(args.j)+'_'+str(args.k)+'/'+pfx+'_'+RecBatchID+'_'+o+'_'+str(args.i)+'_'+str(args.j)+'_'+str(args.k)+'_'+str(args.l)+sfx
-# print(UI.TimeStamp(),'Writing the output...')
-# _Rec_Hits_Pool.to_csv(output_file_location,index=False) #Write the final result
-# print(UI.TimeStamp(),'Output is written to ',output_file_location)
-# exit()
 
 
--- Code/Utilities/U_HC.py
+++ Code/Utilities/U_HC.py
@@ -39,21 +39,25 @@ class HitCluster:
            print('Initial number of all possible hit combinations is:',len(_Hits)**2)
            print('Number of all possible hit combinations without self-permutations:',(len(_Hits)**2)-len(_Hits))
            print('Number of all possible hit  combinations with enforced one-directionality:',int(((len(_Hits)**2)-len(_Hits))/2))
-           n_edg=len(self.RawClusterGraph)
-           job_iter=0
-           acc_edg=0
-           start_pos=0
-           end_pos=n_edg
-           for n_e in range(1,n_edg+1):
-               acc_edg+=n_edg-n_e
-               if acc_edg>=MaxEdges:
-                   job_iter+=1
-                   acc_edg=0
-                   if job_iter==l+1:
-                      end_pos=n_e
-                      break
-                   else:
-                      start_pos=n_e
+           if l>-1 and MaxEdges>-1:
+               n_edg=len(self.RawClusterGraph)
+               job_iter=0
+               acc_edg=0
+               start_pos=0
+               end_pos=n_edg
+               for n_e in range(1,n_edg+1):
+                   acc_edg+=n_edg-n_e
+                   if acc_edg>=MaxEdges:
+                       job_iter+=1
+                       acc_edg=0
+                       if job_iter==l+1:
+                          end_pos=n_e
+                          break
+                       else:
+                          start_pos=n_e
+           else:
+               start_pos=0
+               end_pos=len(_Hits)-1
            for l in range(start_pos,min(end_pos,len(_Hits)-1)):
                for r in range(l+1,len(_Hits)):
                    if HitCluster.JoinHits(_Hits[l],_Hits[r],cut_dt,cut_dr,cut_dz):
@@ -78,8 +82,6 @@ class HitCluster:
                del TH[1:6]
                del TH[2:7]
            self.RawEdgeGraph=_Tot_Hits
-           print(self.RawEdgeGraph)
-           exit()
            return True
       def GenerateEdgGraph(self, cut_dt, cut_dr, cut_dz, MCHits): #Decorate hit information
            #New workaround: instead of a painful Pandas outer join a loop over list is performed
