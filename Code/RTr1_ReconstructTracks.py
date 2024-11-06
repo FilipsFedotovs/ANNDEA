038b660ae58b3b1cdb80218bfe84605c8596da0c
Filips <fedotovs@hotmail.com>
2024-11-06 16:48:15 +0000

    ANNDEA: Redistribute edge generation

--- Code/RTr1_ReconstructTracks.py
+++ Code/RTr1_ReconstructTracks.py
@@ -421,7 +421,12 @@ while Status<len(Program):
         print(bcolors.HEADER+"#############################################################################################"+bcolors.ENDC)
         print(UI.TimeStamp(),bcolors.BOLD+'Stage 1:'+bcolors.ENDC+' Consolidating the edge generation files from the previous step...')
         print(Program[0][1][8])
-        exit()
+        for i in range(len(Program[0][1][8])):
+            for j in range(len(Program[0][1][8][i])):
+                for k in range(len(Program[0][1][8][i][j])):
+                    master_file=EOS_DIR+Program[0][1][3]+'/Temp_'+Program[0][1][5]+'_'+RecBatchID+'_'+str(i)+'_'+str(j)+'_'+str(k)+'/'+Program[0][1][5]+'_'+RecBatchID+'_'+Program[0][1][4]+'_'+str(i)+'_'+str(j)+'_'+str(k)+'_0.pkl'
+                    print(master_file)
+                    exit()
     elif Status==4:
       #Non standard processes (that don't follow the general pattern) have been coded here
       print(bcolors.HEADER+"#############################################################################################"+bcolors.ENDC)
