82f8474382ed369fcc54c59663287364ea319793
Filips <fedotovs@hotmail.com>
2025-01-11 19:14:52 +0000

    ANNDEA: Move consolidation to HTCondor

--- Code/Utilities/RTr1b_ConsolidateEdges_Sub.py
+++ Code/Utilities/RTr1b_ConsolidateEdges_Sub.py
@@ -40,28 +40,16 @@ Program=ast.literal_eval(args.GraphProgram)
 
 i,j,k=args.i,args.j,args.k
 import U_UI as UI #This is where we keep routine utility functions
-
-
 master_file=EOS_DIR+p+'/Temp_RTr1a_'+RecBatchID+'_'+str(i)+'_'+str(j)+'_'+str(k)+'/RTr1a_'+RecBatchID+'_hit_cluster_edges_'+str(i)+'_'+str(j)+'_'+str(k)+'_0.pkl'
-print(master_file)
 master_data=UI.PickleOperations(master_file,'r','')[0]
-x=input()
-for l in range(1,Program[0][1][8][i][j][k]):
-    slave_file=EOS_DIR+p+'/Temp_'+Program[0][1][5]+'_'+RecBatchID+'_'+str(i)+'_'+str(j)+'_'+str(k)+'/'+Program[0][1][5]+'_'+RecBatchID+'_'+Program[0][1][4]+'_'+str(i)+'_'+str(j)+'_'+str(k)+'_'+str(l)+'.pkl'
+for l in range(1,Program[i][j][k]):
+    slave_file=EOS_DIR+p+'/Temp_RTr1a_'+RecBatchID+'_'+str(i)+'_'+str(j)+'_'+str(k)+'/RTr1a_'+RecBatchID+'_hit_cluster_edges_'+str(i)+'_'+str(j)+'_'+str(k)+'_'+str(l)+'.pkl'
     slave_data=UI.PickleOperations(slave_file,'r','')[0]
     master_data.RawEdgeGraph+=slave_data.RawEdgeGraph
     master_data.HitPairs+=slave_data.HitPairs
-output_file=EOS_DIR+Program[2][1][3]+'/Temp_'+Program[2][1][5]+'_'+RecBatchID+'_'+str(i)+'_'+str(j)+'/'+Program[0][1][5]+'_'+RecBatchID+'_'+Program[0][1][4]+'_'+str(i)+'_'+str(j)+'_'+str(k)+'.pkl'
-print(UI.PickleOperations(output_file,'w',master_data)[1])
-
-input_file_location=EOS_DIR+p+'/RTr1_'+RecBatchID+'_'+str(i)+'_'+str(j)+'_'+str(k)+'_clusters.pkl'
-HC=UI.PickleOperations(input_file_location,'r','')[0]
-print(UI.TimeStamp(),'Generating the edges...')
-GraphStatus = HC.GenerateEdges(cut_dt, cut_dr, cut_dz, [], int(l), MaxEdgesPerJob)
 print(UI.TimeStamp(),'Writing the output...')
-output_file_location=EOS_DIR+p+'/Temp_'+pfx+'_'+RecBatchID+'_'+str(i)+'_'+str(j)+'_'+str(k)+'/'+pfx+'_'+RecBatchID+'_'+o+'_'+str(i)+'_'+str(j)+'_'+str(k)+'_'+str(l)+sfx
-UI.PickleOperations(output_file_location,'w',HC)
-print(UI.TimeStamp(),'Output is written to ',output_file_location)
+output_file_location=EOS_DIR+p+'/Temp_'+pfx+'_'+RecBatchID+'_'+str(i)+'_'+str(j)+'_'+str(k)+'/'+pfx+'_'+RecBatchID+'_'+o+'_'+str(i)+'_'+str(j)+'_'+str(k)+'.sfx'
+print(UI.PickleOperations(master_data,'w',master_data)[1])
 exit()
 
 
