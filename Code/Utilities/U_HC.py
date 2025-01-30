b4ce2ce364d44537ad61214c84d28be5ef4c2113
Filips <fedotovs@hotmail.com>
2025-01-30 20:04:16 +0000

    ANNDEA v2 - Include Seed Analysis: MTr1 development

--- Code/Utilities/U_HC.py
+++ Code/Utilities/U_HC.py
@@ -201,7 +201,7 @@ class HitCluster:
           return True
 
       def FitTrackSeed(_H1, _H2, _cdt, _cdr, _cdz): #A more involved option that involves producing the seed cutflow and the truth distribution if the MC data available.
-          _ts=int(((_H1==_H2) and ('--' not in _H1)))
+          _ts=int(((_H1[6]==_H2[6]) and ('--' not in _H1[6])))
           if _H1[3]==_H2[3]: #Ensuring hit combinations are on different plates
                 return False, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
           elif abs(_H1[3]-_H2[3])>=_cdz:
