d809c3075d58e5262e994b3d71269de3fa566b89
Filips <fedotovs@hotmail.com>
2025-02-05 18:21:23 +0000

    ANNDEA v2 - Include Seed Analysis: M2 development

--- Code/Utilities/MTr2_TrainSeedModel_Sub.py
+++ Code/Utilities/MTr2_TrainSeedModel_Sub.py
@@ -102,6 +102,7 @@ def train(model,  sampleX, sampleY, optimizer):
         if len(x)==0: continue
         iterator+=1
         w = model(x)
+        print(w)
         y, w = y.float(), w.squeeze(1)
         #edge weight loss
         loss_w = F.binary_cross_entropy(w, y, reduction='mean')
