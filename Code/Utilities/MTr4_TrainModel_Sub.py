ac2d0379a9bf15adc611adbf593985b2e6448e76
Filips <fedotovs@hotmail.com>
2025-03-16 14:22:58 +0000

    ANNDEA | MTr4 | Working on the GNN tracker trainer

--- Code/Utilities/MTr4_TrainModel_Sub.py
+++ Code/Utilities/MTr4_TrainModel_Sub.py
@@ -102,7 +102,7 @@ def train(model, device, sample, optimizer):
     for HC in sample:
         data = HC.to(device)
         print(data.edge_index)
-        print(data.edge_index.item())
+        print(data.edge_index.size())
         if (len(data.x)==0 or len(data.edge_index)==0): continue
 
         iterator+=1
