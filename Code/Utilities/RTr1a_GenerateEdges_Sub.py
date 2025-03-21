0bfb6fdc9f64c6ff7f28b66a425ee92138955f98
Filips <fedotovs@hotmail.com>
2025-03-21 18:49:25 +0000

    ANNDEA | RTr1 | Working on the GNN tracker

--- Code/Utilities/RTr1a_GenerateEdges_Sub.py
+++ Code/Utilities/RTr1a_GenerateEdges_Sub.py
@@ -56,7 +56,6 @@ GraphStatus = HC.GenerateSeeds(cut_dt, cut_dr, cut_dz, int(l), MaxEdgesPerJob, S
 print(HC.SeedFlowLabels)
 print(HC.SeedFlowValuesAll)
 print(HC.SeedFlowValuesTrue)
-exit()
 print(UI.TimeStamp(),'Writing the output...')
 output_file_location=EOS_DIR+p+'/Temp_'+pfx+'_'+RecBatchID+'_'+str(i)+'_'+str(j)+'_'+str(k)+'/'+pfx+'_'+RecBatchID+'_'+o+'_'+str(i)+'_'+str(j)+'_'+str(k)+'_'+str(l)+sfx
 UI.PickleOperations(output_file_location,'w',HC)
