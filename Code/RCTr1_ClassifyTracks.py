4132598e02535a4d043c8c98217d31d19b91b877
FilipsFedotovs <fedotovs@hotmail.com>
2026-04-15 11:50:27 +0100

    RCTr: ROT - Pandas migration

--- Code/RCTr1_ClassifyTracks.py
+++ Code/RCTr1_ClassifyTracks.py
@@ -133,7 +133,7 @@ if os.path.isfile(required_file_location)==False or Mode=='RESET':
             
         if initial_input_file_location[-5:]=='.root':
              
-            # import ROOT as r
+            import ROOT as r
             # print(UI.TimeStamp(),'Loading the ROOT file content',bcolors.OKBLUE+initial_input_file_location+bcolors.ENDC)
             rdf = r.RDataFrame("tracks",initial_input_file_location)
             # print(UI.TimeStamp(),'Importing data into the Pandas data frame...')
