372834e202347f25c4aa4c5b1f153efc68809197
FilipsFedotovs <fedotovs@hotmail.com>
2026-04-15 10:31:08 +0100

    RCTr: ROT - Pandas migration

--- Code/RCTr1_ClassifyTracks.py
+++ Code/RCTr1_ClassifyTracks.py
@@ -137,7 +137,7 @@ if os.path.isfile(required_file_location)==False or Mode=='RESET':
             print(UI.TimeStamp(),'Loading the ROOT file content',bcolors.OKBLUE+initial_input_file_location+bcolors.ENDC)
             rdf = r.RDataFrame("tracks",initial_input_file_location)
             print(UI.TimeStamp(),'Importing data into the Pandas data frame...')
-            print(rdf.AsNumpy(columns = ["s.eID","s.eX","s.eY","s.eZ","s.eTX","s.eTY",TrackID]))
+            print(rdf.AsNumpy())
             exit()
             if BrickID=='Brick_ID':
                
