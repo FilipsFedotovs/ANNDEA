51832e306df45febb42eca43823c4b9e90836528
FilipsFedotovs <fedotovs@hotmail.com>
2026-04-16 16:24:02 +0100

    RCTr: ROT - Pandas migration

--- Code/RCTr1_ClassifyTracks.py
+++ Code/RCTr1_ClassifyTracks.py
@@ -187,6 +187,9 @@ if os.path.isfile(required_file_location)==False or Mode=='RESET':
             print('A list of plates and the number of tracks starting on them is listed bellow:')
             print(data_aggregated_show.to_string())
             RPChoice = input('Enter the list of plates separated by comma that you want to remove followed by "Enter" : ')
+            RPChoice=ast.literal_eval(RPChoice)
+            print(RPChoice)
+            exit()
             if len(RPChoice)>1:
                 RPChoice=ast.literal_eval(RPChoice)
             else:
