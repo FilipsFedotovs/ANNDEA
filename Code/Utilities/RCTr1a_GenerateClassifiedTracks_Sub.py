27152249566cf9c22385971ba8148672b2a64e7a
FilipsFedotovs <fedotovs@hotmail.com>
2026-04-28 17:08:24 +0100

    RCTr: ROT - Pandas migration - change py path

--- Code/Utilities/RCTr1a_GenerateClassifiedTracks_Sub.py
+++ Code/Utilities/RCTr1a_GenerateClassifiedTracks_Sub.py
@@ -47,7 +47,7 @@ PY_DIR=args.PY
 if PY_DIR!='': #Temp solution
     sys.path=['',PY_DIR]
 #     sys.path.append('/usr/lib64/python39.zip')
-#     sys.path.append('/usr/lib64/python3.9')
+    sys.path.append('/usr/lib64/python3.9')
 #     sys.path.append('/usr/lib64/python3.9/lib-dynload')
 #     sys.path.append('/usr/lib64/python3.9/site-packages')
 #     sys.path.append('/usr/lib/python3.9/site-packages')
