6f1bf3569ee1ec85ccf9d790c8049a64c6b9a42f
FilipsFedotovs <fedotovs@hotmail.com>
2026-04-29 16:19:07 +0100

    RCTr: ROT - Pandas migration - change py path

--- Code/RCTr1_ClassifyTracks.py
+++ Code/RCTr1_ClassifyTracks.py
@@ -247,10 +247,7 @@ if os.path.isfile(required_file_location)==False or Mode=='RESET':
         final_rows=len(data.axes[0])
 
         print(UI.TimeStamp(),'After removing tracks with number of hits we have',final_rows,' hits left')
-        track_no_data=data.groupby(['Rec_Seg_ID'],as_index=False).count()
-        print(track_no_data)
-        exit()
-        track_no_data=track_no_data.drop([PM.y,PM.z,PM.tx,PM.ty],axis=1)
+        track_no_data=data.groupby(['Rec_Seg_ID']).agg({PM.x:'count'}).reset_index()
         track_no_data=track_no_data.rename(columns={PM.x: "Track_No"})
         new_combined_data=pd.merge(data, track_no_data, how="left", on=["Rec_Seg_ID"])
         new_combined_data = new_combined_data[new_combined_data.Track_No >= MinHitsTrack]
