4665f7c4af1d5122fa5c4b4d600b93f31ff12e0b
FilipsFedotovs <fedotovs@hotmail.com>
2026-05-12 22:56:53 +0100

    RCTr: ROT - Pandas migration - change py path

--- Code/RCTr1_ClassifyTracks.py
+++ Code/RCTr1_ClassifyTracks.py
@@ -346,6 +346,7 @@ while Status<len(Program):
                 print(UI.TimeStamp(),'Loading the ROOT file content',bcolors.OKBLUE+initial_input_file_location+bcolors.ENDC)
                 rdf = r.RDataFrame("tracks",initial_input_file_location)
                 print(rdf.GetColumnNames())
+                print(rdf.Display(["s.ePID"], 10).Print())
                 print(UI.TimeStamp(),'Importing data into the Pandas data frame...')
                 
 
